677d56171814daeffec32f9db135a0585a033f93
Matthew Petroff <matthew@mpetroff.net>
2013-09-16 15:46:30 -0400

    Fix 3d box document unit change undo bug.
    (bzr r12475.1.26)

--- src/sp-item-group.cpp
+++ src/sp-item-group.cpp
@@ -601,7 +601,9 @@ void SPGroup::scaleChildItemsRec(Geom::Scale const &sc, Geom::Point const &p)
                             item->removeAttribute("inkscape:connector-type");
                         }
                         
-                        if ((SP_IS_TEXT_TEXTPATH(item) || SP_IS_FLOWTEXT(item)) && !item->transform.isIdentity()) {
+                        if (SP_IS_PERSP3D(item)) {
+                            persp3d_apply_affine_transformation(SP_PERSP3D(item), final);
+                        } else if ((SP_IS_TEXT_TEXTPATH(item) || SP_IS_FLOWTEXT(item)) && !item->transform.isIdentity()) {
                             // Save and reset current transform
                             Geom::Affine tmp(item->transform);
                             item->transform = Geom::Affine();
