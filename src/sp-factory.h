7d61a9ad9a59c2b78e9ba391bfab6e09b87de7c1
Markus Engel <markus.engel@tum.de>
2013-09-16 20:33:53 +0200

    Added gpl notice
    (bzr r11608.1.125)

--- src/sp-factory.h
+++ src/sp-factory.h
@@ -1,3 +1,13 @@
+/** @file
+ * Factory for SPObject tree
+ *//*
+ * Authors:
+ *   Markus Engel
+ *
+ * Copyright (C) 2013 Authors
+ * Released under GNU GPL, read the file 'COPYING' for more information
+ */
+
 #ifndef SP_FACTORY_SEEN
 #define SP_FACTORY_SEEN
 
@@ -8,3 +18,14 @@ typedef Singleton< Factory<SPObject> > SPFactory;
 
 
 #endif
+
+/*
+  Local Variables:
+  mode:c++
+  c-file-style:"stroustrup"
+  c-file-offsets:((innamespace . 0)(inline-open . 0)(case-label . +))
+  indent-tabs-mode:nil
+  fill-column:99
+  End:
+*/
+// vim: filetype=cpp:expandtab:shiftwidth=4:tabstop=8:softtabstop=4:fileencoding=utf-8:textwidth=99 :
