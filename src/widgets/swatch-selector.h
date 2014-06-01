65782d9a28fe5745e672b33c005edfda715c2ed8
Tomasz Boczkowski <penginsbacon@gmail.com>
2014-06-01 16:41:20 +0200

    using ColorSelector in SwatchSelector
    (bzr r13341.6.50)

--- src/widgets/swatch-selector.h
+++ src/widgets/swatch-selector.h
@@ -10,6 +10,7 @@
 #endif
 
 #include <gtkmm/box.h>
+#include "ui/selected-color.h"
 
 class SPDocument;
 class SPGradient;
@@ -37,13 +38,14 @@ public:
     SPGradientSelector *getGradientSelector();
 
 private:
-    static void _grabbedCb(SPColorSelector *csel, void *data);
-    static void _draggedCb(SPColorSelector *csel, void *data);
-    static void _releasedCb(SPColorSelector *csel, void *data);
-    static void _changedCb(SPColorSelector *csel, void *data);
+    void _grabbedCb();
+    void _draggedCb();
+    void _releasedCb();
+    void _changedCb();
 
     SPGradientSelector *_gsel;
-    SPColorSelector *_csel;
+    Inkscape::UI::SelectedColor _selected_color;
+    bool _updating_color;
 };
 
 
