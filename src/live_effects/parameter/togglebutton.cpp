db11af074f242faa77a1763efc54d26752fa6d93
Jabier Arraiza Cenoz <jabier.arraiza@marker.es>
2014-07-22 18:55:10 +0200

    Fixed alignaments of toggle buttons
    (bzr r13341.1.95)

--- src/live_effects/parameter/togglebutton.cpp
+++ src/live_effects/parameter/togglebutton.cpp
@@ -95,12 +95,12 @@ ToggleButtonParam::param_newWidget()
             iconButton = sp_icon_new(iconSize, iconInactive);
         }
         gtk_widget_show(iconButton);
-        gtk_box_pack_start (GTK_BOX(boxButton), iconButton, true, true, 2);
+        gtk_box_pack_start (GTK_BOX(boxButton), iconButton, false, false, 1);
         if (!param_label.empty()) {
-            gtk_box_pack_start (GTK_BOX(boxButton), labelButton, true, true, 2);
+            gtk_box_pack_start (GTK_BOX(boxButton), labelButton, false, false, 1);
         }
     }else{
-        gtk_box_pack_start (GTK_BOX(boxButton), labelButton, true, true, 2);
+        gtk_box_pack_start (GTK_BOX(boxButton), labelButton, false, false, 1);
     }
     checkwdg->add(*Gtk::manage(Glib::wrap(boxButton)));
     checkwdg->setActive(value);
