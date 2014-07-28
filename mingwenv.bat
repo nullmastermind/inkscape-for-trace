5487ec386162f6c662232a7d34360895800b2d06
Johan B. C. Engelen <jbc.engelen@swissonline.ch>
2014-07-28 22:11:22 +0200

    revert unintentional change in last commit.
    (bzr r13341.1.104)

--- mingwenv.bat
+++ mingwenv.bat
@@ -1,6 +1,6 @@
 @echo Setting environment variables for MinGw build of Inkscape
-IF "%DEVLIBS_PATH%"=="" set DEVLIBS_PATH=c:\devlibs64
-IF "%MINGW_PATH%"=="" set MINGW_PATH=C:\mingw64\mingw64_posix_sjlj
+IF "%DEVLIBS_PATH%"=="" set DEVLIBS_PATH=c:\devlibs
+IF "%MINGW_PATH%"=="" set MINGW_PATH=c:\mingw
 set MINGW_BIN=%MINGW_PATH%\bin
 set PKG_CONFIG_PATH=%DEVLIBS_PATH%\lib\pkgconfig
 set GS_BIN=C:\latex\gs\gs8.61\bin
