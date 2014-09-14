db956ebd6ec994674645cb88d2dfc819d7da2cd6
su_v <suv-sf@users.sourceforge.net>
2014-09-14 22:15:13 +0200

    fix flawed test for cpu capability (r13600)
    (bzr r13506.1.95)

--- packaging/macosx/osx-app.sh
+++ packaging/macosx/osx-app.sh
@@ -282,8 +282,7 @@ ARCH="$(uname -a | awk '{print $NF;}')"
 
 # guess default build_arch (MacPorts)
 if [ "$OSXMINORNO" -ge "6" ]; then
-	_cpu_bits="$(sysctl hw.cpu64bit_capable > /dev/null 2>&1)"
-	if [ $? -eq 0 ]; then
+	if [ "$(sysctl hw.cpu64bit_capable 2>/dev/null | awk '{print $NF;}')" = "1" ]; then
 		_build_arch="x86_64"
 	else
 		_build_arch="i386"
