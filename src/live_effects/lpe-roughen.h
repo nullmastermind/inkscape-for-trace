d4e5309ab79434f518ab55fe6b47ff185ca80e62
Jabier Arraiza Cenoz <jabier.arraiza@marker.es>
2014-09-15 18:50:10 +0200

    Roughen LPE: cleanup, improvements in code -loop part thanks Nathan-, credits and license fixed
    (bzr r13341.1.208)

--- src/live_effects/lpe-roughen.h
+++ src/live_effects/lpe-roughen.h
@@ -1,11 +1,17 @@
-#ifndef INKSCAPE_LPE_ROUGHEN_H
-#define INKSCAPE_LPE_ROUGHEN_H
-
-/*
- * Inkscape::LPERoughen
+/** @file
+ * @brief Roughen LPE effect, see lpe-roughen.cpp.
+ */
+/* Authors:
+ *   Jabier Arraiza Cenoz <jabier.arraiza@marker.es>
+ *
+ * Copyright (C) 2014 Authors
  *
  * Released under GNU GPL, read the file 'COPYING' for more information
  */
+
+#ifndef INKSCAPE_LPE_ROUGHEN_H
+#define INKSCAPE_LPE_ROUGHEN_H
+
 #include "live_effects/parameter/enum.h"
 #include "live_effects/effect.h"
 #include "live_effects/parameter/parameter.h"
@@ -18,37 +24,37 @@ namespace Inkscape {
 namespace LivePathEffect {
 
 enum DivisionMethod {
-  DM_SEGMENTS,
-  DM_SIZE,
-  DM_END
+    DM_SEGMENTS,
+    DM_SIZE,
+    DM_END
 };
 
 class LPERoughen : public Effect {
 
 public:
-  LPERoughen(LivePathEffectObject *lpeobject);
-  virtual ~LPERoughen();
+    LPERoughen(LivePathEffectObject *lpeobject);
+    virtual ~LPERoughen();
 
-  virtual void doEffect(SPCurve *curve);
-  virtual double sign(double randNumber);
-  virtual Geom::Point randomize();
-  virtual void doBeforeEffect(SPLPEItem const */*lpeitem*/);
-  virtual SPCurve *addNodesAndJitter(const Geom::Curve *A, double t);
-  virtual SPCurve *jitter(const Geom::Curve *A);
-  virtual Geom::Point tpoint(Geom::Point A, Geom::Point B, double t = 0.5);
-  virtual Gtk::Widget *newWidget();
+    virtual void doEffect(SPCurve *curve);
+    virtual double sign(double randNumber);
+    virtual Geom::Point randomize();
+    virtual void doBeforeEffect(SPLPEItem const */*lpeitem*/);
+    virtual SPCurve *addNodesAndJitter(const Geom::Curve *A, double t);
+    virtual SPCurve *jitter(const Geom::Curve *A);
+    virtual Geom::Point tpoint(Geom::Point A, Geom::Point B, double t = 0.5);
+    virtual Gtk::Widget *newWidget();
 
 private:
-  UnitParam unit;
-  EnumParam<DivisionMethod> method;
-  ScalarParam maxSegmentSize;
-  ScalarParam segments;
-  RandomParam displaceX;
-  RandomParam displaceY;
-  BoolParam shiftNodes;
-  BoolParam shiftNodeHandles;
-  LPERoughen(const LPERoughen &);
-  LPERoughen &operator=(const LPERoughen &);
+    UnitParam unit;
+    EnumParam<DivisionMethod> method;
+    ScalarParam maxSegmentSize;
+    ScalarParam segments;
+    RandomParam displaceX;
+    RandomParam displaceY;
+    BoolParam shiftNodes;
+    BoolParam shiftNodeHandles;
+    LPERoughen(const LPERoughen &);
+    LPERoughen &operator=(const LPERoughen &);
 
 };
 
