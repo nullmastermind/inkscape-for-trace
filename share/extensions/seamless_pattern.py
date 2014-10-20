0e3a5980d8304716a94a5bddba8c5a9ebcf739b2
Jabier Arraiza Cenoz <jabier.arraiza@marker.es>
2014-10-20 21:35:49 +0200

    Fix a bug getting a template file in the seamless extension, also upgrade this extension to a newer version
    (bzr r13341.1.283)

--- share/extensions/seamless_pattern.py
+++ share/extensions/seamless_pattern.py
@@ -1,7 +1,7 @@
 #!/usr/bin/env python
 
 # Written by Jabiertxof
-# V.03
+# V.04
 
 import inkex
 import re
@@ -26,170 +26,116 @@ class C(inkex.Effect):
     root.set("height", str(height) + 'px')
     root.set("viewBox", "0 0 " + str(width) + " " + str(height) )
 
-    xpathStr = '//svg:use[@id="sampleTile"]'
-    sampleTile = root.xpath(xpathStr, namespaces=inkex.NSS)
-    if sampleTile != []:
-        sampleTile[0].set("transform", "translate(" + str(width * 6) + ",-" + str(height) + ")")
-        
-        
-    xpathStr = '//svg:use[@id="clipPathRect"]'
+    xpathStr = '//svg:rect[@id="clipPathRect"]'
     clipPathRect = root.xpath(xpathStr, namespaces=inkex.NSS)
     if clipPathRect != []:
-        clipPathRect[0].set("transform", "translate(" + str((width*3)) + "," + str(height) +")")
+        clipPathRect[0].set("width", str(width))
+        clipPathRect[0].set("height", str(height))
 
-    xpathStr = '//svg:tspan[@id="infoText"]'
-    infoText = root.xpath(xpathStr, namespaces=inkex.NSS)
-    if infoText != []:
-        infoText[0].text = infoText[0].text.replace("100x100", str(width) + "x" + str(height))
-
-    xpathStr = '//svg:use[@id="backgroundPattern"]'
-    backgroundPattern = root.xpath(xpathStr, namespaces=inkex.NSS)
-    if backgroundPattern != []:
-        backgroundPattern[0].set("transform", "translate(-" + str(width * 2) + ",0)")
-
-    xpathStr = '//svg:rect[@id="cuadroPattern"]'
-    cuadroPattern = root.xpath(xpathStr, namespaces=inkex.NSS)
-    if cuadroPattern != []:
-        cuadroPattern[0].set("width", str(width))
-        cuadroPattern[0].set("height", str(height))
-        cuadroPattern[0].set("y", "-" + str(height*2))
-
-    xpathStr = '//svg:use[@id="cuadroPatternDisenador"]'
-    cuadroPatternDisenador = root.xpath(xpathStr, namespaces=inkex.NSS)
-    if cuadroPatternDisenador != []:
-        cuadroPatternDisenador[0].set("transform", "translate(" + str(width * 3) + "," + str(height) + ")")
-
-    xpathStr = '//svg:g[@id="designZone"]'
+    xpathStr = '//svg:g[@id="designTop"] | //svg:g[@id="designBottom"]'
     designZone = root.xpath(xpathStr, namespaces=inkex.NSS)
     if designZone != []:
-        designZone[0].set("transform", "translate(" + str((width*5)) + ",0)scale(" + str(((width+height)/2)/100.) + ")")
+        designZone[0].set("transform", "scale(" + str(width/100.) + ","+ str(height/100.) + ")")
+        designZone[1].set("transform", "scale(" + str(width/100.) + ","+ str(height/100.) + ")")
 
-    xpathStr = '//svg:g[@id="patternResult"]'
-    patternResult = root.xpath(xpathStr, namespaces=inkex.NSS)
-    if patternResult != []:
-        patternResult[0].set("transform", "translate(-" + str((width * 3)) + "," + str(height) + ")")
-
-    xpathStr = '//svg:use[@id="patternTileBackground"]'
-    patternTileBackground = root.xpath(xpathStr, namespaces=inkex.NSS)
-    if patternTileBackground != []:
-        patternTileBackground[0].set("transform", "matrix(3,0,0,3," + str((width * 2)) + "," + str(height * 4) + ")")
-        patternTileBackground[0].set("width", str(width*3))
-        patternTileBackground[0].set("height", str(height*3))
-
-    xpathStr = '//svg:use[@id="patternGenerated"]'
-    patternGenerated = root.xpath(xpathStr, namespaces=inkex.NSS)
-    if patternGenerated != []:
-        patternGenerated[0].set("width", str(width))
-        patternGenerated[0].set("height", str(height))
-
-    xpathStr = '//svg:use[@id="pattern1"]'
+    xpathStr = '//svg:use[@id="top1"] | //svg:use[@id="bottom1"]'
     pattern1 = root.xpath(xpathStr, namespaces=inkex.NSS)
     if pattern1 != []:
         pattern1[0].set("transform", "translate(" + str(-width) + "," + str(-height) + ")")
+        pattern1[1].set("transform", "translate(" + str(-width) + "," + str(-height) + ")")
 
-    xpathStr = '//svg:use[@id="pattern2"]'
+    xpathStr = '//svg:use[@id="top2"] | //svg:use[@id="bottom2"]'
     pattern2 = root.xpath(xpathStr, namespaces=inkex.NSS)
     if pattern2 != []:
         pattern2[0].set("transform", "translate(0," + str(-height) +")" )
+        pattern2[1].set("transform", "translate(0," + str(-height) +")" )
 
-    xpathStr = '//svg:use[@id="pattern3"]'
+    xpathStr = '//svg:use[@id="top3"] | //svg:use[@id="bottom3"]'
     pattern3 = root.xpath(xpathStr, namespaces=inkex.NSS)
     if pattern3 != []:
         pattern3[0].set("transform", "translate(" + str(width) + "," + str(-height) + ")" )
+        pattern3[1].set("transform", "translate(" + str(width) + "," + str(-height) + ")" )
 
-    xpathStr = '//svg:use[@id="pattern4"]'
+    xpathStr = '//svg:use[@id="top4"] | //svg:use[@id="bottom4"]'
     pattern4 = root.xpath(xpathStr, namespaces=inkex.NSS)
     if pattern4 != []:
         pattern4[0].set("transform", "translate(" + str(-width) + ",0)" )
+        pattern4[1].set("transform", "translate(" + str(-width) + ",0)" )
 
-    xpathStr = '//svg:use[@id="pattern5"]'
+    xpathStr = '//svg:use[@id="top5"] | //svg:use[@id="bottom5"]'
     pattern5 = root.xpath(xpathStr, namespaces=inkex.NSS)
     if pattern5 != []:
         pattern5[0].set("transform", "translate(0,0)" )
+        pattern5[1].set("transform", "translate(0,0)" )
 
-    xpathStr = '//svg:use[@id="pattern6"]'
+    xpathStr = '//svg:use[@id="top6"] | //svg:use[@id="bottom6"]'
     pattern6 = root.xpath(xpathStr, namespaces=inkex.NSS)
     if pattern6 != []:
         pattern6[0].set("transform", "translate(" + str(width) + ",0)" )
+        pattern6[1].set("transform", "translate(" + str(width) + ",0)" )
 
-    xpathStr = '//svg:use[@id="pattern7"]'
+    xpathStr = '//svg:use[@id="top7"] | //svg:use[@id="bottom7"]'
     pattern7 = root.xpath(xpathStr, namespaces=inkex.NSS)
     if pattern7 != []:
         pattern7[0].set("transform", "translate(" + str(-width) + "," + str(height) + ")" )
+        pattern7[1].set("transform", "translate(" + str(-width) + "," + str(height) + ")" )
 
-    xpathStr = '//svg:use[@id="pattern8"]'
+    xpathStr = '//svg:use[@id="top8"] | //svg:use[@id="bottom8"]'
     pattern8 = root.xpath(xpathStr, namespaces=inkex.NSS)
     if pattern8 != []:
         pattern8[0].set("transform", "translate(0," + str(height) + ")" )
+        pattern8[1].set("transform", "translate(0," + str(height) + ")" )
 
-    xpathStr = '//svg:use[@id="pattern9"]'
+    xpathStr = '//svg:use[@id="top9"] | //svg:use[@id="bottom9"]'
     pattern9 = root.xpath(xpathStr, namespaces=inkex.NSS)
     if pattern9 != []:
         pattern9[0].set("transform", "translate(" + str(width) + "," + str(height) + ")" )
+        pattern9[1].set("transform", "translate(" + str(width) + "," + str(height) + ")" )
 
-    xpathStr = '//svg:use[@id="samplePattern1"]'
-    samplePattern1 = root.xpath(xpathStr, namespaces=inkex.NSS)
-    if samplePattern1 != []:
-        samplePattern1[0].set("transform", "translate(" + str(-width) + "," + str(-height) + ")")
+    xpathStr = '//svg:use[@id="clonePreview1"]'
+    clonePreview1 = root.xpath(xpathStr, namespaces=inkex.NSS)
+    if clonePreview1 != []:
+        clonePreview1[0].set("transform", "translate(0," + str(height) + ")" )
 
-    xpathStr = '//svg:use[@id="samplePattern2"]'
-    samplePattern2 = root.xpath(xpathStr, namespaces=inkex.NSS)
-    if samplePattern2 != []:
-        samplePattern2[0].set("transform", "translate(0," + str(-height) +")" )
+    xpathStr = '//svg:use[@id="clonePreview2"]'
+    clonePreview2 = root.xpath(xpathStr, namespaces=inkex.NSS)
+    if clonePreview2 != []:
+        clonePreview2[0].set("transform", "translate(0," + str(height * 2) + ")" )
 
-    xpathStr = '//svg:use[@id="samplePattern3"]'
-    samplePattern3 = root.xpath(xpathStr, namespaces=inkex.NSS)
-    if samplePattern3 != []:
-        samplePattern3[0].set("transform", "translate(" + str(width) + "," + str(-height) + ")" )
+    xpathStr = '//svg:use[@id="clonePreview3"]'
+    clonePreview3 = root.xpath(xpathStr, namespaces=inkex.NSS)
+    if clonePreview3 != []:
+        clonePreview3[0].set("transform", "translate(" + str(width) + ",0)" )
 
-    xpathStr = '//svg:use[@id="samplePattern4"]'
-    samplePattern4 = root.xpath(xpathStr, namespaces=inkex.NSS)
-    if samplePattern4 != []:
-        samplePattern4[0].set("transform", "translate(" + str(-width) + ",0)" )
+    xpathStr = '//svg:use[@id="clonePreview4"]'
+    clonePreview4 = root.xpath(xpathStr, namespaces=inkex.NSS)
+    if clonePreview4 != []:
+        clonePreview4[0].set("transform", "translate(" + str(width) + "," + str(height) + ")" )
 
-    xpathStr = '//svg:use[@id="samplePattern5"]'
-    samplePattern5 = root.xpath(xpathStr, namespaces=inkex.NSS)
-    if samplePattern5 != []:
-        samplePattern5[0].set("transform", "translate(0,0)" )
+    xpathStr = '//svg:use[@id="clonePreview5"]'
+    clonePreview5 = root.xpath(xpathStr, namespaces=inkex.NSS)
+    if clonePreview5 != []:
+        clonePreview5[0].set("transform", "translate(" + str(width) + "," + str(height * 2) + ")" )
 
-    xpathStr = '//svg:use[@id="samplePattern6"]'
-    samplePattern6 = root.xpath(xpathStr, namespaces=inkex.NSS)
-    if samplePattern6 != []:
-        samplePattern6[0].set("transform", "translate(" + str(width) + ",0)" )
+    xpathStr = '//svg:use[@id="clonePreview6"]'
+    clonePreview6 = root.xpath(xpathStr, namespaces=inkex.NSS)
+    if clonePreview6 != []:
+        clonePreview6[0].set("transform", "translate(" + str(width*2) + ", 0)" )
 
-    xpathStr = '//svg:use[@id="samplePattern7"]'
-    samplePattern7 = root.xpath(xpathStr, namespaces=inkex.NSS)
-    if samplePattern7 != []:
-        samplePattern7[0].set("transform", "translate(" + str(-width) + "," + str(height) + ")" )
+    xpathStr = '//svg:use[@id="clonePreview7"]'
+    clonePreview7 = root.xpath(xpathStr, namespaces=inkex.NSS)
+    if clonePreview7 != []:
+        clonePreview7[0].set("transform", "translate(" + str(width*2) + "," + str(height) + ")" )
 
-    xpathStr = '//svg:use[@id="samplePattern8"]'
-    samplePattern8 = root.xpath(xpathStr, namespaces=inkex.NSS)
-    if samplePattern8 != []:
-        samplePattern8[0].set("transform", "translate(0," + str(height) + ")" )
+    xpathStr = '//svg:use[@id="clonePreview8"]'
+    clonePreview8 = root.xpath(xpathStr, namespaces=inkex.NSS)
+    if clonePreview8 != []:
+        clonePreview8[0].set("transform", "translate(" + str(width*2) + "," + str(height*2) + ")" )
 
-    xpathStr = '//svg:use[@id="samplePattern9"]'
-    samplePattern9 = root.xpath(xpathStr, namespaces=inkex.NSS)
-    if samplePattern9 != []:
-        samplePattern9[0].set("transform", "translate(" + str(width) + "," + str(height) + ")" )
-
-    xpathStr = '//svg:g[@id="patternPreview"]'
-    patternPreview = root.xpath(xpathStr, namespaces=inkex.NSS)
-    if patternPreview != []:
-        patternPreview[0].set("transform", "translate(0," + str(height*2) + ")" )
-
-    xpathStr = '//svg:g[@id="helperLayer"]'
-    helperLayer = root.xpath(xpathStr, namespaces=inkex.NSS)
-    if helperLayer != []:
-        helperLayer[0].set("transform", "scale(" + str(width/100.) + "," + str(height/100.) + ")" )
-
-    xpathStr = '//svg:rect[@id="fondoGris"]'
-    fondoGris = root.xpath(xpathStr, namespaces=inkex.NSS)
-    if fondoGris != []:
-        fondoGris[0].set("transform", "scale(" + str(width/100.) + "," + str(height/100.) + ")" )
-
-    xpathStr = '//svg:use[@id="patternGenerator"  or id="patternPreviewFast"]'
+    xpathStr = '//svg:use[@id="fullPatternClone"]'
     patternGenerator = root.xpath(xpathStr, namespaces=inkex.NSS)
     if patternGenerator != []:
+        patternGenerator[0].set("transform", "translate(" + str(width * 2) + ",-" + str(height) + ")" )
         patternGenerator[0].set("{http://www.inkscape.org/namespaces/inkscape}tile-cx", str(width/2))
         patternGenerator[0].set("{http://www.inkscape.org/namespaces/inkscape}tile-cy", str(height/2))
         patternGenerator[0].set("{http://www.inkscape.org/namespaces/inkscape}tile-w", str(width))
