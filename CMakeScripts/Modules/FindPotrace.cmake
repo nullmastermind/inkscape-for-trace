ac1a99494b6afaf5673f260f0e75559eff3b5ff5
Alex Valavanis <valavanisalex@gmail.com>
2015-11-12 22:35:18 +0000

    Update cmake Potrace module to match style of others
    (bzr r14449.1.5)

--- CMakeScripts/Modules/FindPotrace.cmake
+++ CMakeScripts/Modules/FindPotrace.cmake
@@ -1,36 +1,62 @@
 #  POTRACE_FOUND - system has Potrace
-#  POTRACE_INCLUDE_DIR - the Potrace include directory
+#  POTRACE_INCLUDE_DIRS - the Potrace include directory
 #  POTRACE_LIBRARIES - The libraries needed to use Potrace
 
-FIND_PATH(POTRACE_INCLUDE_DIR potracelib.h
-   /usr/include
-   /usr/local/include
-)
+IF (POTRACE_LIBRARIES AND POTRACE_INCLUDE_DIRS)
+   # in cache already
+   SET(POTRACE_FOUND TRUE)
+ELSE (POTRACE_LIBRARIES AND POTRACE_INCLUDE_DIRS)
+   FIND_PATH (POTRACE_INCLUDE_DIR
+      NAMES
+         potracelib.h
+      PATHS
+         /usr/include
+         /usr/local/include
+	 $ENV{DEVLIBS_PATH}/include
+      PATH_SUFFIXES
+         potrace
+   )
 
-FIND_LIBRARY(POTRACE_LIBRARY NAMES  potrace libpotrace
-   PATHS
-   /usr/lib
-   /usr/local/lib
-)
+   FIND_LIBRARY (POTRACE_LIBRARY
+      NAMES
+        potrace
+        libpotrace
+      PATHS
+         /usr/lib
+         /usr/local/lib
+         $ENV{DEVLIBS_PATH}/lib
+   )
 
-if (POTRACE_INCLUDE_DIR AND POTRACE_LIBRARY)
-   set(POTRACE_FOUND TRUE)
-   set(POTRACE_LIBRARIES ${POTRACE_LIBRARY})
-else (POTRACE_INCLUDE_DIR AND POTRACE_LIBRARY)
-   set(POTRACE_FOUND FALSE)
-endif (POTRACE_INCLUDE_DIR AND POTRACE_LIBRARY)
+   if (POTRACE_LIBRARY)
+      set (POTRACE_FOUND TRUE)
+   endif (POTRACE_LIBRARY)
 
-if (POTRACE_FOUND)
-   if (NOT Potrace_FIND_QUIETLY)
-      message(STATUS "Found potrace: ${POTRACE_LIBRARIES}")
-   endif (NOT Potrace_FIND_QUIETLY)
-else (POTRACE_FOUND)
-   if (NOT Potrace_FIND_QUIETLY)
+   set (POTRACE_INCLUDE_DIRS
+      ${POTRACE_INCLUDE_DIR}
+   )
 
- message(STATUS "don't find potrace")
+   if (POTRACE_FOUND)
+      set(POTRACE_LIBRARIES
+        ${POTRACE_LIBRARIES}
+        ${POTRACE_LIBRARY}
+      )
+   endif (POTRACE_FOUND)
 
-   endif (NOT Potrace_FIND_QUIETLY)
-endif (POTRACE_FOUND)
+   if (POTRACE_INCLUDE_DIRS AND POTRACE_LIBRARIES)
+      set(POTRACE_FOUND TRUE)
+   endif (POTRACE_INCLUDE_DIRS AND POTRACE_LIBRARIES)
 
-MARK_AS_ADVANCED(POTRACE_INCLUDE_DIR POTRACE_LIBRARIES POTRACE_LIBRARY)
+   if (POTRACE_FOUND)
+      if (NOT Potrace_FIND_QUIETLY)
+         message(STATUS "Found Potrace: ${POTRACE_LIBRARIES}")
+      endif (NOT Potrace_FIND_QUIETLY)
+   else (POTRACE_FOUND)
+      if (Potrace_FIND_REQUIRED)
+	      message(FATAL_ERROR "Could not find potrace")
+      endif (Potrace_FIND_REQUIRED)
+   endif (POTRACE_FOUND)
 
+   # show the POTRACE_INCLUDE_DIRS and POTRACE_LIBRARIES variables only in the advanced view
+   MARK_AS_ADVANCED(POTRACE_INCLUDE_DIRS POTRACE_LIBRARIES)
+
+endif (POTRACE_LIBRARIES AND POTRACE_INCLUDE_DIRS)
