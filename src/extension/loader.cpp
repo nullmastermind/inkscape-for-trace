fd70a7878e170d18f91e06a28b48e1a63daf216b
Moritz Eberl <moritz@semiodesk.com>
2016-04-12 17:23:50 +0200

    Modified the windows build to integrate gmodule-2.0 and loader.cpp/.h Added method to check inscape version used to build plugins.
    (bzr r14761.1.3)

--- src/extension/loader.cpp
+++ src/extension/loader.cpp
@@ -14,11 +14,13 @@
 #include <exception>
 #include <string.h>
 #include "dependency.h"
+#include "inkscape-version.h"
 
 namespace Inkscape {
 namespace Extension {
 
 typedef Implementation::Implementation *(*_getImplementation)(void);
+typedef const gchar *(*_getInkscapeVersion)(void);
 
 bool Loader::load_dependency(Dependency *dep)
 {
@@ -38,43 +40,75 @@ bool Loader::load_dependency(Dependency *dep)
 Implementation::Implementation *Loader::load_implementation(Inkscape::XML::Document *doc)
 {
     try {
+        
         Inkscape::XML::Node *repr = doc->root();
         Inkscape::XML::Node *child_repr = repr->firstChild();
+        
+        // Iterate over the xml content
         while (child_repr != NULL) {
             char const *chname = child_repr->name();
             if (!strncmp(chname, INKSCAPE_EXTENSION_NS_NC, strlen(INKSCAPE_EXTENSION_NS_NC))) {
                 chname += strlen(INKSCAPE_EXTENSION_NS);
             }
-
+            
+            // Deal with dependencies if we have them
             if (!strcmp(chname, "dependency")) {
                 Dependency dep = Dependency(child_repr);
+                // try to load it
                 bool success = load_dependency(&dep);
                 if( !success ){
+                    // Could not load dependency, we abort
                     const char *res = g_module_error();
                     g_warning("Unable to load dependency %s of plugin %s.\nDetails: %s\n", dep.get_name(), res);
                     return NULL;
                 }
             } 
 
+            // Found a plugin to load
             if (!strcmp(chname, "plugin")) {
+                
+                // The name of the plugin is actually the library file we want to load
                 if (const gchar *name = child_repr->attribute("name")) {
                     GModule *module = NULL;
                     _getImplementation GetImplementation = NULL;
+                    _getInkscapeVersion GetInkscapeVersion = NULL;
+                    
+                    // build the path where to look for the plugin
                     gchar *path = g_build_filename(_baseDirectory, name, (char *) NULL);
                     module = g_module_open(path, G_MODULE_BIND_LOCAL);
                     g_free(path);
+                    
                     if (module == NULL) {
+                        // we were not able to load the plugin, write warning and abort
                         const char *res = g_module_error();
                         g_warning("Unable to load extension %s.\nDetails: %s\n", name, res);
                         return NULL;
                     }
 
+                    // Get a handle to the version function of the module
+                    if (g_module_symbol(module, "GetInkscapeVersion", (gpointer *) &GetInkscapeVersion) == FALSE) {
+                        // This didn't work, write warning and abort
+                        const char *res = g_module_error();
+                        g_warning("Unable to load extension %s.\nDetails: %s\n", name, res);
+                        return NULL;
+                    }
+                    
+                    // Get a handle to the function that delivers the implementation
                     if (g_module_symbol(module, "GetImplementation", (gpointer *) &GetImplementation) == FALSE) {
+                        // This didn't work, write warning and abort
                         const char *res = g_module_error();
                         g_warning("Unable to load extension %s.\nDetails: %s\n", name, res);
                         return NULL;
                     }
-
+                    
+                    // Get version and test against this version
+                    const gchar* version = GetInkscapeVersion();
+                    if( strcmp(version, version_string) != 0) {
+                        // The versions are different, display warning.
+                        g_warning("Plugin was built against Inkscape version %s, this is %s. The plugin might not be compatible.", version, version_string);
+                    }
+                    
+                    
                     Implementation::Implementation *i = GetImplementation();
                     return i;
                 }
