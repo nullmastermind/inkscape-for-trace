7b0f2601a652adb7566bfac6ec0eab7a9d95c7a2
Sebastian Faubel <sebastian@semiodesk.com>
2016-07-06 17:42:00 +0200

    Fixing build for Linux. Replaced tabs with whitespaces.
    (bzr r15002.1.7)

--- mingwenv.cmake
+++ mingwenv.cmake
@@ -17,31 +17,31 @@ set(ENV_GS_PATH C:/latex/gs/gs8.61)
 # MinGW Configuration
 # -----------------------------------------------------------------------------
 message(STATUS "Configuring MinGW environment:")
-	
+  
 if("$ENV{DEVLIBS_PATH}" STREQUAL "")
-	message(STATUS "  Setting path to development libraries from mingwenv.cmake: ${ENV_DEVLIBS_PATH}")
-	set(DEVLIBS_PATH ${ENV_DEVLIBS_PATH})
+  message(STATUS "  Setting path to development libraries from mingwenv.cmake: ${ENV_DEVLIBS_PATH}")
+  set(DEVLIBS_PATH ${ENV_DEVLIBS_PATH})
 else()
-	message(STATUS "  Setting path to development libraries from environment: $ENV{DEVLIBS_PATH}")
-	set(DEVLIBS_PATH $ENV{DEVLIBS_PATH})
+  message(STATUS "  Setting path to development libraries from environment: $ENV{DEVLIBS_PATH}")
+  set(DEVLIBS_PATH $ENV{DEVLIBS_PATH})
 endif()
 
 if("$ENV{MINGW_PATH}" STREQUAL "")
-	message(STATUS "  Setting path to MinGW from mingwenv.cmake: ${ENV_MINGW_PATH}")
-	set(MINGW_PATH ${ENV_MINGW_PATH})
+  message(STATUS "  Setting path to MinGW from mingwenv.cmake: ${ENV_MINGW_PATH}")
+  set(MINGW_PATH ${ENV_MINGW_PATH})
 else()
-	message(STATUS "  Setting path to MinGW from environment: $ENV{MINGW_PATH}")
-	set(MINGW_PATH $ENV{MINGW_PATH})
+  message(STATUS "  Setting path to MinGW from environment: $ENV{MINGW_PATH}")
+  set(MINGW_PATH $ENV{MINGW_PATH})
 endif()
 
 if("$ENV{GS_PATH}" STREQUAL "")
-	message(STATUS "  Setting path to Ghostscript from mingwenv.cmake: ${ENV_GS_PATH}")
-	set(GS_PATH ${ENV_GS_PATH})
+  message(STATUS "  Setting path to Ghostscript from mingwenv.cmake: ${ENV_GS_PATH}")
+  set(GS_PATH ${ENV_GS_PATH})
 else()
-	message(STATUS "  Setting path to Ghostscript from environment: $ENV{GS_PATH}")
-	set(GS_PATH $ENV{GS_PATH})
+  message(STATUS "  Setting path to Ghostscript from environment: $ENV{GS_PATH}")
+  set(GS_PATH $ENV{GS_PATH})
 endif()
-	
+  
 # Normalize directory separator slashes.
 string(REGEX REPLACE "\\\\" "/" DEVLIBS_PATH ${DEVLIBS_PATH})
 string(REGEX REPLACE "\\\\" "/" MINGW_PATH ${MINGW_PATH})
@@ -55,7 +55,7 @@ string(REGEX REPLACE "\\\\" "/" GS_PATH ${GS_PATH})
 set(DEVLIBS_LIB "${DEVLIBS_PATH}/lib")
 
 if(NOT EXISTS "${DEVLIBS_LIB}")
-	message(FATAL_ERROR "Inkscape development libraries directory does not exist: ${DEVLIBS_LIB}")
+  message(FATAL_ERROR "Inkscape development libraries directory does not exist: ${DEVLIBS_LIB}")
 endif()
 
 # Add devlibs libraries to linker path.
@@ -64,7 +64,7 @@ link_directories(${DEVLIBS_LIB})
 set(DEVLIBS_INCLUDE ${DEVLIBS_PATH}/include)
 
 if(NOT EXISTS ${DEVLIBS_INCLUDE})
-	message(FATAL_ERROR "Inkscape development libraries directory does not exist: ${DEVLIBS_INCLUDE}")
+  message(FATAL_ERROR "Inkscape development libraries directory does not exist: ${DEVLIBS_INCLUDE}")
 endif()
 
 # Add general MinGW headers to compiler include path.
@@ -74,14 +74,14 @@ endif()
 set(DEVLIBS_BIN ${DEVLIBS_PATH}/bin)
 
 if(NOT EXISTS ${DEVLIBS_BIN})
-	message(FATAL_ERROR "Inkscape development binaries directory does not exist: ${DEVLIBS_BIN}")
+  message(FATAL_ERROR "Inkscape development binaries directory does not exist: ${DEVLIBS_BIN}")
 endif()
 
 # Directory containing the pkgconfig .pc files.
 set(PKG_CONFIG_PATH "${DEVLIBS_PATH}/lib/pkgconfig")
 
 if(NOT EXISTS "${PKG_CONFIG_PATH}")
-	message(FATAL_ERROR "pkgconfig directory does not exist: ${PKG_CONFIG_PATH}")
+  message(FATAL_ERROR "pkgconfig directory does not exist: ${PKG_CONFIG_PATH}")
 endif()
 
 # Add the devlibs directories to the paths used to find libraries and programs.
@@ -96,16 +96,16 @@ list(APPEND CMAKE_PROGRAM_PATH  ${DEVLIBS_BIN})
 
 # We are in a MinGW environment.
 set(HAVE_MINGW ON)
-	
+  
 # Try to determine the MinGW processor architecture.
 if(EXISTS "${MINGW_PATH}/mingw32")
-	set(HAVE_MINGW64 OFF)
-	set(MINGW_ARCH mingw32)
+  set(HAVE_MINGW64 OFF)
+  set(MINGW_ARCH mingw32)
 elseif(EXISTS "${MINGW_PATH}/x86_64-w64-mingw32")
-	set(HAVE_MINGW64 ON)
-	set(MINGW_ARCH x86_64-w64-mingw32)
+  set(HAVE_MINGW64 ON)
+  set(MINGW_ARCH x86_64-w64-mingw32)
 else()
-	message(FATAL_ERROR "Unable to determine MinGW processor architecture. Are you using an unsupported MinGW version?")
+  message(FATAL_ERROR "Unable to determine MinGW processor architecture. Are you using an unsupported MinGW version?")
 endif()
 
 # Path to processor architecture specific binaries and libs.
@@ -114,7 +114,7 @@ set(MINGW_ARCH_PATH "${MINGW_PATH}/${MINGW_ARCH}")
 set(MINGW_BIN "${MINGW_PATH}/bin")
 
 if(NOT EXISTS ${MINGW_BIN})
-	message(FATAL_ERROR "MinGW binary directory does not exist: ${MINGW_BIN}")
+  message(FATAL_ERROR "MinGW binary directory does not exist: ${MINGW_BIN}")
 endif()
 
 # Eliminate error messages when not having mingw in the environment path variable.
@@ -123,7 +123,7 @@ list(APPEND CMAKE_PROGRAM_PATH  ${MINGW_BIN})
 set(MINGW_LIB "${MINGW_PATH}/lib")
 
 if(NOT EXISTS ${MINGW_LIB})
-	message(FATAL_ERROR "MinGW library directory does not exist: ${MINGW_LIB}")
+  message(FATAL_ERROR "MinGW library directory does not exist: ${MINGW_LIB}")
 endif()
 
 # Add MinGW libraries to linker path.
@@ -132,30 +132,30 @@ link_directories(${MINGW_LIB})
 set(MINGW_INCLUDE "${MINGW_PATH}/include")
 
 if(NOT EXISTS ${MINGW_INCLUDE})
-	message(FATAL_ERROR "MinGW include directory does not exist: ${MINGW_INCLUDE}")
+  message(FATAL_ERROR "MinGW include directory does not exist: ${MINGW_INCLUDE}")
 endif()
 
 # Add general MinGW headers to compiler include path.
 include_directories(SYSTEM ${MINGW_INCLUDE})
 
 if(HAVE_MINGW64)
-	set(MINGW64_LIB "${MINGW_ARCH_PATH}/lib")
-	
-	if(NOT EXISTS ${MINGW64_LIB})
-		message(FATAL_ERROR "MinGW 64-Bit libraries directory does not exist: ${MINGW64_LIB}")
-	endif()
+  set(MINGW64_LIB "${MINGW_ARCH_PATH}/lib")
+  
+  if(NOT EXISTS ${MINGW64_LIB})
+    message(FATAL_ERROR "MinGW 64-Bit libraries directory does not exist: ${MINGW64_LIB}")
+  endif()
 
-	# Add 64-Bit libraries to linker path.
-	link_directories(${MINGW64_LIB})
-	
-	set(MINGW64_INCLUDE "${MINGW_ARCH_PATH}/include")
-	
-	if(NOT EXISTS ${MINGW64_INCLUDE})
-		message(FATAL_ERROR "MinGW 64-Bit include directory does not exist: ${MINGW64_INCLUDE}")
-	endif()
+  # Add 64-Bit libraries to linker path.
+  link_directories(${MINGW64_LIB})
+  
+  set(MINGW64_INCLUDE "${MINGW_ARCH_PATH}/include")
+  
+  if(NOT EXISTS ${MINGW64_INCLUDE})
+    message(FATAL_ERROR "MinGW 64-Bit include directory does not exist: ${MINGW64_INCLUDE}")
+  endif()
 
-	# Add 64-Bit MinGW headers to compiler include path.
-	include_directories(${MINGW64_INCLUDE})
+  # Add 64-Bit MinGW headers to compiler include path.
+  include_directories(${MINGW64_INCLUDE})
 endif()
 
 # -----------------------------------------------------------------------------
@@ -166,9 +166,9 @@ endif()
 set(GS_BIN "${GS_PATH}/bin")
 
 if(EXISTS "${GS_BIN}")
-	set(HAVE_GS_BIN ON)
+  set(HAVE_GS_BIN ON)
 else()
-	set(HAVE_GS_BIN OFF)
+  set(HAVE_GS_BIN OFF)
 endif()
 
 # -----------------------------------------------------------------------------
@@ -180,5 +180,5 @@ set(CMAKE_FIND_LIBRARY_PREFIXES "lib")
 set(CMAKE_FIND_LIBRARY_SUFFIXES ".dll.a" ".dll")
 
 if(NOT HAVE_MINGW64)
-	list(APPEND CMAKE_FIND_LIBRARY_SUFFIXES ".a")
+  list(APPEND CMAKE_FIND_LIBRARY_SUFFIXES ".a")
 endif()
