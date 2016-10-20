181667844fb6862fb0bbfee5e91601188047d7ca
Eduard Braun <eduard.braun2@gmx.de>
2016-10-20 21:51:31 +0200

    Packaging: Support for additional components in WIX installer It's now possible to select specific translations and to choose wether or not to install Python, extensions and dictionaries.
    (bzr r15244.1.17)

--- packaging/wix/files.py
+++ packaging/wix/files.py
@@ -3,9 +3,10 @@
 from __future__ import print_function
 
 import os
+import re
 import uuid
 
-from helpers import get_inkscape_dist_dir
+from helpers import get_inkscape_dist_dir, get_inkscape_locales_and_names
 
 directory_ids = {}
 file_ids = {}
@@ -13,8 +14,11 @@ file_ids = {}
 def indent(level):
 	indentstring = ''
 	for i in range(level):
-		indentstring += '    '
+		indentstring += '	'
 	return indentstring
+
+def valid_id(identifier):
+	return identifier.replace('@','_')
 
 def directory(root, breadcrumb, level, exclude=[]):
 	"""
@@ -36,7 +40,7 @@ def directory(root, breadcrumb, level, exclude=[]):
 		wxs.write(indent(level + 1)+ "<File Id='file" + _id + "' Name='" + file + "' DiskId='1' Source='" + file_key + "' KeyPath='yes' />\n")
 		wxs.write(indent(level)+ "</Component>\n")
 	# then all directories
-	
+
 	dirs = [ f for f in os.listdir(root) if os.path.isdir(os.path.join(root,f)) ]
 	for dir in dirs:
 		directory_key = breadcrumb + '__' + dir
@@ -45,15 +49,32 @@ def directory(root, breadcrumb, level, exclude=[]):
 		wxs.write(indent(level) + "<Directory Id='" + directory_ids[directory_key] + "' Name='" + dir + "'>\n")
 		directory(os.path.join(root, dir), directory_key, level + 1)
 		wxs.write(indent(level) + "</Directory>\n")
-	
-	
-def ComponentGroup(name, condition, level):
+
+def test_conditions(value, conditions):
+	"""
+	check if "value" fullfills any of the "conditions", where a condition can be
+	   - a string that has to be a substring of "value"
+	   - a compiled regex pattern which has to match in "value"
+	"""
+	if not isinstance(conditions, list):
+		conditions = [conditions]
+	for condition in conditions:
+		if isinstance(condition, basestring):
+			if condition in value:
+				return True
+		elif isinstance(condition, type(re.compile(''))):
+			if re.search(condition, value):
+				return True
+	return False
+
+
+def ComponentGroup(name, conditions, level):
 	"""
 	add componentgroup that contain all items from file_ids that match condition
 	remove the matched elements from file_ids
 	"""
 	global file_ids
-	keys = [k for k in file_ids.keys() if condition in k]
+	keys = [k for k in file_ids.keys() if test_conditions(k, conditions)]
 	wxs.write(indent(level) + "<ComponentGroup Id='" + name + "'>\n")
 	for component in keys:
 		wxs.write(indent(level + 1) + "<ComponentRef Id='" + file_ids[component] + "' />\n")
@@ -61,8 +82,11 @@ def ComponentGroup(name, condition, level):
 	for key in keys:
 		del file_ids[key]
 
-#get directory containing the inkscape distribution files
+# get directory containing the Inkscape distribution files
 inkscape_dist_dir = get_inkscape_dist_dir()
+
+# get locales currently supported by Inkscape (dict of the form {'de': 'German (de)'})
+locales = get_inkscape_locales_and_names()
 
 with open('files.wxs', 'w') as wxs:
 	wxs.write("<!-- do not edit, this file is created by files.py tool any changes will be lost -->\n")
@@ -78,18 +102,33 @@ with open('files.wxs', 'w') as wxs:
 	print("found %d files" % len(file_ids.keys()))
 	wxs.write(indent(4) + "</Directory>\n")
 	wxs.write(indent(3) + "</Directory>\n")
-	# link to ProgrmMenu
+	# link to ProgramMenu
 	wxs.write(indent(3) + "<Directory Id='ProgramMenuFolder'>\n")
 	wxs.write(indent(4) + "<Directory Id='ApplicationProgramsFolder' Name='$(var.FullProductName)'/>\n")
 	wxs.write(indent(3) + "</Directory>\n")
 	wxs.write(indent(3) + "<Directory Id='DesktopFolder' Name='Desktop' />\n")
 	wxs.write(indent(2) + "</Directory>\n")
-	ComponentGroup("Examples", 'inkscape\\share\\examples', 2)
+
+	# Python
+	ComponentGroup("Python", 'inkscape\\python\\', 2)
+	# translations and localized content
+	for lang_code in sorted(locales):
+		ComponentGroup("Translation_" + valid_id(lang_code), ['\\' + lang_code + '\\',
+		                                                      re.compile(r'\.' + lang_code + r'\.[a-z]+$')], 2)
+	# other components
+	ComponentGroup("Extensions", 'inkscape\\share\\extensions\\', 2)
+	ComponentGroup("Examples", 'inkscape\\share\\examples\\', 2)
 	ComponentGroup("Tutorials", 'inkscape\\share\\tutorials\\', 2)
-	ComponentGroup("Translations", '\\locale\\', 2)
+	ComponentGroup("Dictionaries", 'inkscape\\lib\\aspell-0.60\\', 2)
+	# everything that is left (which should be the Inkscape core unless inkscape_dist_dir contains unnecessary files or we defined our components poorly)
 	ComponentGroup("AllOther", '', 2)
+
+	# create a FeatureGroup for translations
+	wxs.write(indent(2) + "<FeatureGroup Id='Translations'>\n")
+	for lang_code in sorted(locales):
+		wxs.write(indent(3) + "<Feature Id='Translation_" + valid_id(lang_code) + "' Level='1' Title='" + locales[lang_code] + "' AllowAdvertise='no'>\n")
+		wxs.write(indent(4) + "<ComponentGroupRef Id='Translation_" + valid_id(lang_code) + "' />\n")
+		wxs.write(indent(3) + "</Feature>\n")
+	wxs.write(indent(2) + "</FeatureGroup>\n")
 	wxs.write(indent(1) + "</Fragment>\n")
 	wxs.write("</Wix>\n")
-
-	
-
