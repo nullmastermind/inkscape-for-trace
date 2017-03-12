4aacbb7d40826888f18238ce8cad134b5dbed934
Alvin Penner <penner@vaxxine.com>
2017-03-12 19:12:23 -0400

    extensions. dxf output & print_win32_vector. compensate for viewbox. (Bug 1672066)
    Fixed bugs:
      - https://launchpad.net/bugs/1672066
    
    (bzr r15590)

--- share/extensions/dxf_outlines.py
+++ share/extensions/dxf_outlines.py
@@ -342,8 +342,15 @@ class MyEffect(inkex.Effect):
             scale = 25.4/96     # if no scale is specified, assume inch as baseunit
         scale /= self.unittouu('1px')
         h = self.unittouu(self.document.getroot().xpath('@height', namespaces=inkex.NSS)[0])
-        self.groupmat = [[[scale, 0.0, 0.0], [0.0, -scale, h*scale]]]
         doc = self.document.getroot()
+        # process viewBox height attribute to correct page scaling
+        viewBox = doc.get('viewBox')
+        if viewBox:
+            viewBox2 = viewBox.split(',')
+            if len(viewBox2) < 4:
+                viewBox2 = viewBox.split(' ')
+            scale *= h / self.unittouu(self.addDocumentUnit(viewBox2[3]))
+        self.groupmat = [[[scale, 0.0, 0.0], [0.0, -scale, h*scale]]]
         self.process_group(doc)
         if self.options.ROBO == 'true':
             self.ROBO_output()
