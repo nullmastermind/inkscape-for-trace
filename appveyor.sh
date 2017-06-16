2e1e4ac91785491c4b730316bbcbe889759595c5
Eduard Braun <Eduard.Braun2@gmx.de>
2017-06-16 18:59:48 +0200

    Install updated versions of libgc (https://github.com/Alexpux/MINGW-packages/pull/2597) and gtest (https://github.com/Alexpux/MINGW-packages/pull/2588) - libgc 7.6.0 solves a deadlocking issue that prevented tests to ever complete (it might also be related to https://bugs.launchpad.net/inkscape/+bug/1659172 and https://bugs.launchpad.net/inkscape/+bug/1655619) - gtest 1.8.0 includes gmock making it unnecessary to download and build separately

--- appveyor.sh
+++ appveyor.sh
@@ -27,6 +27,14 @@ source ../msys2installdeps.sh
 pacman -S $MINGW_PACKAGE_PREFIX-ccache --needed --noconfirm --noprogressbar
 ccache --max-size=200M
 
+# pending package updates, see
+#   - https://github.com/Alexpux/MINGW-packages/pull/2597
+#   - https://github.com/Alexpux/MINGW-packages/pull/2588
+wget -nv https://gitlab.com/Ede123/bintray/raw/master/$MINGW_PACKAGE_PREFIX-gc-7.6.0-1-any.pkg.tar.xz \
+    && pacman -U $MINGW_PACKAGE_PREFIX-gc-7.6.0-1-any.pkg.tar.xz --noconfirm
+wget -nv https://gitlab.com/Ede123/bintray/raw/master/$MINGW_PACKAGE_PREFIX-gtest-1.8.0-1-any.pkg.tar.xz \
+    && pacman -U $MINGW_PACKAGE_PREFIX-gtest-1.8.0-1-any.pkg.tar.xz --noconfirm
+
 
 
 ### build / test
