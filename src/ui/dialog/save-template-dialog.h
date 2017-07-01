af269da07f7d7b60c30abfe4f78610f752c27dbb
Christophe Lebras <christophe.lebras@gmail.com>
2017-07-01 14:28:24 +0000

    Add Save Template Dialog
    This add just dialog widget only, don't save template yet.

---
+++ src/ui/dialog/save-template-dialog.h
@@ -0,0 +1,51 @@
+#ifndef INKSCAPE_SEEN_UI_DIALOG_SAVE_TEMPLATE_H
+#define INKSCAPE_SEEN_UI_DIALOG_SAVE_TEMPLATE_H
+
+#include <gtkmm/dialog.h>
+#include <gtkmm/entry.h>
+#include <gtkmm/box.h>
+#include <gtkmm/grid.h>
+#include <gtkmm/label.h>
+#include <gtkmm/checkbutton.h>
+
+namespace Inkscape {
+namespace UI {
+namespace Dialog {
+
+class SaveTemplate : public Gtk::Dialog
+{
+
+public:
+
+    static void save_document_as_template();
+
+protected:
+
+    void on_name_changed();
+
+private:
+
+    Gtk::Grid grid;
+
+    Gtk::Label name_label;
+    Gtk::Entry name_text;
+
+    Gtk::Label author_label;
+    Gtk::Entry author_text;
+
+    Gtk::Label description_label;
+    Gtk::Entry description_text;
+
+    Gtk::Label keywords_label;
+    Gtk::Entry keywords_text;
+
+    Gtk::CheckButton is_default_template;
+
+    SaveTemplate();
+    void save_template();
+
+};
+}
+}
+}
+#endif
