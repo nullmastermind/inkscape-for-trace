nullmastermind/inkscape-for-trace

commit 2abe0bb681044d972e171189395a2afdbc39bf28

Fix inverted y sign for glyph positioning
This is causing vertical glyph positioning to move in the wrong
direction e.g. marks go below base glyph instead of above. Seems to be a
regression introduced in:

commit 83dac189ff21c59be9b4f912e0d0e9690e710a4d
Author: Tavmjong Bah <tavmjong@free.fr>
Date:   Tue Nov 3 13:19:36 2015 +0100

    Rearrange code to make handling of baseline clearer.

    (bzr r14430.1.3)

src/libnrtype/Layout-TNG-Compute.cpp
@@ -767,7 +767,7 @@ void Layout::Calculator::_outputLine(ParagraphInfo const &para,
                             if( dominant_baseline == SP_CSS_BASELINE_AUTO ) dominant_baseline = SP_CSS_BASELINE_ALPHABETIC;
                         }
 
-                        new_glyph.y += delta_y;
+                        new_glyph.y -= delta_y;
 
                         // TODO: Should also check 'glyph_orientation_vertical' if 'text-orientation' is unset...
                         if( new_span.text_orientation == SP_CSS_TEXT_ORIENTATION_SIDEWAYS ||
@@ -801,7 +801,7 @@ void Layout::Calculator::_outputLine(ParagraphInfo const &para,
 
                         if( dominant_baseline == SP_CSS_BASELINE_AUTO ) dominant_baseline = SP_CSS_BASELINE_ALPHABETIC;
 
-                        new_glyph.y -= delta_y;
+                        new_glyph.y += delta_y;
                         new_glyph.y += new_span.font_size * para.pango_items[unbroken_span.pango_item_index].font->GetBaselines()[ dominant_baseline ];
                         
                         new_glyph.width = unbroken_span_glyph_info->geometry.width * font_size_multiplier;

testfiles/rendering_tests/CMakeLists.txt
@@ -2,6 +2,7 @@
 #add your test here (do not put the .svg extension)
 set(RENDERING_TESTS 
     test-empty
+    test-glyph-y-pos
 )
 
 

testfiles/rendering_tests/test-glyph-y-pos.svg
@@ -1,0 +1,26 @@
+<?xml version="1.0" encoding="UTF-8" standalone="no"?>
+
+<svg width="600" height="600"
+   xmlns="http://www.w3.org/2000/svg"
+   xmlns:xlink="http://www.w3.org/1999/xlink"
+   version="1.1"
+   viewBox="0 0 600 600">
+  <style type="text/css">
+    text {
+      font-family: DejaVu Sans;
+      font-size: 36px;
+    }
+    .title {
+      text-anchor: middle;
+    }
+  </style>
+
+  <g>
+    <text x="50"  y="200">G̃g̃X̃x̃</text>
+    <text x="300" y="200" style="writing-mode:vertical-lr;">G̃g̃X̃x̃</text>
+    <text x="500" y="200" style="writing-mode:vertical-lr;text-orientation:upright">G̃g̃X̃x̃</text>
+  </g>
+
+  <text class="title" x="50%" y="120">Composed Glyphs</text>
+
+</svg>

commit e564b8bfdedb3655fe47f47996fdd778a0e75d7b

Fix baseline shift movement direction
This was broke in 2abe0bb681044d972e171189395a2afdbc39bf28 when delta_y
was applied in the opposite Y direction but baseline_shift was kept
unchanged.

src/libnrtype/Layout-TNG-Compute.cpp
@@ -779,7 +779,7 @@ void Layout::Calculator::_outputLine(ParagraphInfo const &para,
                     // y-coordinate is flipped between vertical and horizontal text...
                     // delta_y is common offset but applied with opposite sign
                     double delta_x = unbroken_span_glyph_info->geometry.x_offset * font_size_multiplier;
-                    double delta_y = unbroken_span_glyph_info->geometry.y_offset * font_size_multiplier + unbroken_span.baseline_shift;
+                    double delta_y = unbroken_span_glyph_info->geometry.y_offset * font_size_multiplier - unbroken_span.baseline_shift;
                     SPCSSBaseline dominant_baseline = _flow._blockBaseline();
 
                     if (_block_progression == LEFT_TO_RIGHT || _block_progression == RIGHT_TO_LEFT) {

testfiles/rendering_tests/CMakeLists.txt
@@ -1,6 +1,7 @@
 
 #add your test here (do not put the .svg extension)
 set(RENDERING_TESTS 
+    test-baseline-shift
     test-empty
     test-glyph-y-pos
     test-rtl-vertical

testfiles/rendering_tests/test-baseline-shift.svg
@@ -1,0 +1,27 @@
+<?xml version="1.0" encoding="UTF-8" standalone="no"?>
+
+<svg width="600" height="600"
+   xmlns="http://www.w3.org/2000/svg"
+   xmlns:xlink="http://www.w3.org/1999/xlink"
+   version="1.1"
+   viewBox="0 0 600 600">
+  <style type="text/css">
+    text {
+      font-family: DejaVu Sans;
+      font-size: 36px;
+    }
+    .title {
+      text-anchor: middle;
+    }
+  </style>
+
+  <g>
+    <text x="50"  y="200">subscript: H<tspan style="font-size:65%;baseline-shift:sub">2</tspan>O</text>
+    <text x="50"  y="300">superscript: m<tspan style="font-size:65%;baseline-shift:super">2</tspan></text>
+    <text x="530" y="200" style="writing-mode:tb-rl">subscript: H<tspan style="font-size:65%;baseline-shift:sub">2</tspan>O</text>
+    <text x="430" y="200" style="writing-mode:tb-rl">superscript: m<tspan style="font-size:65%;baseline-shift:super">2</tspan></text>
+  </g>
+
+  <text class="title" x="50%" y="120">Sub- and Superscript</text>
+
+</svg>