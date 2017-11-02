cba70f5669c6a38321157fbb84a8511033b7b41e
Jabier Arraiza <jabier.arraiza@marker.es>
2017-11-02 21:14:20 +0100

    Remove unrelated code

--- src/live_effects/lpe-powermask.h
+++ src/live_effects/lpe-powermask.h
@@ -6,11 +6,10 @@
  *
  * Released under GNU GPL, read the file 'COPYING' for more information
  */
+
 #include "live_effects/effect.h"
-#include "live_effects/parameter/bool.h"
 #include "live_effects/parameter/text.h"
 #include "live_effects/parameter/hidden.h"
-#include "live_effects/parameter/colorpicker.h"
 
 namespace Inkscape {
 namespace LivePathEffect {
@@ -23,17 +22,17 @@ public:
     virtual void doEffect (SPCurve * curve);
     virtual void doOnRemove (SPLPEItem const* /*lpeitem*/);
     virtual void doOnVisibilityToggled(SPLPEItem const* lpeitem);
+    //virtual void transform_multiply(Geom::Affine const& postmul, bool set);
     void toggleMaskVisibility();
     void setMask();
 private:
     HiddenParam uri;
     BoolParam invert;
-    //BoolParam wrap;
+    BoolParam wrap;
     BoolParam hide_mask;
     BoolParam background;
-    ColorPickerParam background_color;
+    TextParam background_style;
     Geom::Path mask_box;
-    guint32 previous_color;
 };
 
 } //namespace LivePathEffect
