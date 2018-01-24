nullmastermind/inkscape-for-trace

commit 6f844ef457690c841b0be91d70b1e54b61c04812

Remove DOM directory and reduce size of inkscape. Use Inkscape::URI and save ziptool to utils.
(bzr r13047.1.5)

src/extension/internal/odf.cpp
@@ -77,12 +77,7 @@
 #include "text-editing.h"
 #include "util/units.h"
 
-
-//# DOM-specific includes
-#include "dom/dom.h"
-#include "dom/util/ziptool.h"
-//#include "dom/io/domstream.h"
-
+#include "uri.h"
 
 #include "inkscape-version.h"
 #include "document.h"
@@ -90,6 +85,7 @@
 
 #include "io/inkscapestream.h"
 #include "io/bufferstream.h"
+#include <util/ziptool.h>
 #include <iomanip>
 namespace Inkscape
 {
@@ -98,7 +94,6 @@ namespace Extension
 namespace Internal
 {
 //# Shorthand notation
-typedef org::w3c::dom::DOMString DOMString;
 typedef Inkscape::IO::BufferOutputStream BufferOutputStream;
 typedef Inkscape::IO::OutputStreamWriter OutputStreamWriter;
 typedef Inkscape::IO::StringOutputStream StringOutputStream;
@@ -1026,6 +1021,7 @@ static void gatherText(Inkscape::XML::Node *node, Glib::ustring &buf)
 
 }
 
+
 /**
  * FIRST PASS.
  * Method descends into the repr tree, converting image, style, and gradient info
@@ -1086,10 +1082,9 @@ void OdfOutput::preprocess(ZipFile &zf, Inkscape::XML::Node *node)
                 imageTable[oldName] = newName;
                 Glib::ustring comment = "old name was: ";
                 comment.append(oldName);
-                URI oldUri(oldName);
+                Inkscape::URI oldUri(oldName.c_str());
                 //# if relative to the documentURI, get proper path
-                URI resUri = documentUri.resolve(oldUri);
-                DOMString pathName = resUri.getNativePath();
+                std::string pathName = documentUri.getFullPath(oldUri.getFullPath(""));
                 ZipEntry *ze = zf.addFile(pathName, comment);
                 if (ze)
                     {
@@ -2105,9 +2100,7 @@ void OdfOutput::save(Inkscape::Extension::Output */*mod*/, SPDocument *doc, gcha
 {
     reset();
 
-    documentUri = URI(filename);
-    /* fixme: It looks like we really are using documentUri as a URI, so we ought to call
-     * g_filename_to_uri for the URI constructor. */
+    documentUri = Inkscape::URI(filename);
 
     ZipFile zf;
     preprocess(zf, doc->rroot);

src/extension/internal/odf.h
@@ -23,18 +23,16 @@
 #ifndef EXTENSION_INTERNAL_ODG_OUT_H
 #define EXTENSION_INTERNAL_ODG_OUT_H
 
-#include <dom/dom.h>
 #include <io/stringstream.h>
-#include <dom/uri.h>
+#include <util/ziptool.h>
 
 #include "extension/implementation/implementation.h"
 
 #include <xml/repr.h>
-
 #include <string>
 #include <map>
 
-#include <dom/util/ziptool.h>
+#include "uri.h"
 #include "sp-item.h"
 
 #include <glibmm/ustring.h>
@@ -46,7 +44,6 @@ namespace Extension
 namespace Internal
 {
 
-typedef org::w3c::dom::URI URI;
 typedef Inkscape::IO::Writer Writer;
 
 class StyleInfo
@@ -279,7 +276,7 @@ public:
 
 private:
 
-    URI documentUri;
+    Inkscape::URI documentUri;
 
     void reset();
 

commit d61e166a900e32e6f1640c7c26b1a8a66826e51d

Fix handling of filenames of linked images in .odg export
- export of .odg files was completely broken on Windows as
  "documentUri = Inkscape::URI(filename);" always failed
  (Windows paths are never valid URIs because of '\')
- Linked images did not seem to be working on any OS
  (probably regression in 6f844ef457690c841b0be91d70b1e54b61c04812)

I assume support for linked images is still sketchy but seems to
work in the most common cases; support for embedded images seems
to be non-existent, though.

Fixed bug:
  - https://bugs.launchpad.net/inkscape/+bug/1654034

src/extension/internal/odf.cpp
@@ -1079,8 +1079,7 @@ void OdfOutput::preprocess(ZipFile &zf, Inkscape::XML::Node *node)
                 Glib::ustring comment = "old name was: ";
                 comment.append(oldName);
                 Inkscape::URI oldUri(oldName.c_str());
-                //# if relative to the documentURI, get proper path
-                std::string pathName = documentUri.getFullPath(oldUri.getFullPath(""));
+                std::string pathName = oldUri.toNativeFilename();
                 ZipEntry *ze = zf.addFile(pathName, comment);
                 if (ze)
                     {
@@ -2095,8 +2094,6 @@ void OdfOutput::reset()
 void OdfOutput::save(Inkscape::Extension::Output */*mod*/, SPDocument *doc, gchar const *filename)
 {
     reset();
-
-    documentUri = Inkscape::URI(filename);
 
     ZipFile zf;
     preprocess(zf, doc->rroot);

src/extension/internal/odf.h
@@ -276,8 +276,6 @@ public:
 
 private:
 
-    Inkscape::URI documentUri;
-
     void reset();
 
     //cc or dc metadata name/value pairs