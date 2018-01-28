842e44d5502627516e91f3c07a5bce3ebe6a2f2b
Eduard Braun <eduard.braun2@gmx.de>
2018-01-28 15:11:47 +0100

    Fix creation of icon-theme.cache
    Escaping "${CMAKE_INSTALL_PREFIX}" with a backslash allows this to
    work for the normal install target as well as with cpack (expansion
    of the variable is delayed until installation where it has the
    correct value).
    
    Also make sure Adwaita's icon-theme.cache is up-to-date

--- CMakeScripts/InstallMSYS2.cmake
+++ CMakeScripts/InstallMSYS2.cmake
@@ -134,6 +134,7 @@ if(WIN32)
 
   install(DIRECTORY ${MINGW_PATH}/share/icons/Adwaita
     DESTINATION share/icons)
+  install(CODE "execute_process(COMMAND gtk-update-icon-cache \${CMAKE_INSTALL_PREFIX}/share/icons/Adwaita)")
 
   # translations for libraries (we usually shouldn't need many)
   file(GLOB inkscape_translations RELATIVE ${CMAKE_SOURCE_DIR}/po/ ${CMAKE_SOURCE_DIR}/po/*.po)
