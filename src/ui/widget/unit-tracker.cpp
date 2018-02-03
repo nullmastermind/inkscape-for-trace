nullmastermind/inkscape-for-trace

commit e0957537cd0938313803c290a2f3922a3889e6f1

Removed all GSList occurences in .h files

src/ui/widget/unit-tracker.cpp
@@ -14,7 +14,7 @@
 
 #include "style-internal.h"
 #include "unit-tracker.h"
-#include "widgets/ege-select-one-action.h"
+//#include "widgets/ege-select-one-action.h"
 
 #define COLUMN_STRING 0
 
@@ -31,9 +31,6 @@ UnitTracker::UnitTracker(UnitType unit_type) :
     _activeUnit(NULL),
     _activeUnitInitialized(false),
     _store(0),
-    _unitList(0),
-    _actionList(0),
-    _adjList(0),
     _priorValues()
 {
     _store = gtk_list_store_new(1, G_TYPE_STRING);
@@ -58,17 +55,17 @@ UnitTracker::UnitTracker(UnitType unit_type) :
 UnitTracker::~UnitTracker()
 {
     // Unhook weak references to GtkActions
-    while (_actionList) {
-        g_signal_handlers_disconnect_by_func(G_OBJECT(_actionList->data), (gpointer) _unitChangedCB, this);
-        g_object_weak_unref(G_OBJECT(_actionList->data), _actionFinalizedCB, this);
-        _actionList = g_slist_delete_link(_actionList, _actionList);
+    for (auto i : _actionList) {
+        g_signal_handlers_disconnect_by_func(G_OBJECT(i), (gpointer) _unitChangedCB, this);
+        g_object_weak_unref(G_OBJECT(i), _actionFinalizedCB, this);
     }
+    _actionList.clear();
 
     // Unhook weak references to GtkAdjustments
-    while (_adjList) {
-        g_object_weak_unref(G_OBJECT(_adjList->data), _adjustmentFinalizedCB, this);
-        _adjList = g_slist_delete_link(_adjList, _adjList);
+    for (auto i : _adjList) {
+        g_object_weak_unref(G_OBJECT(i), _adjustmentFinalizedCB, this);
     }
+    _adjList.clear();
 }
 
 bool UnitTracker::isUpdating() const
@@ -109,9 +106,9 @@ void UnitTracker::setActiveUnitByAbbr(gchar const *abbr)
 
 void UnitTracker::addAdjustment(GtkAdjustment *adj)
 {
-    if (!g_slist_find(_adjList, adj)) {
+    if (std::find(_adjList.begin(),_adjList.end(),adj)!=_adjList.end()) {
         g_object_weak_ref(G_OBJECT(adj), _adjustmentFinalizedCB, this);
-        _adjList = g_slist_append(_adjList, adj);
+        _adjList.push_back(adj);
     }
 }
 
@@ -147,7 +144,7 @@ GtkAction *UnitTracker::createAction(gchar const *name, gchar const *label, gcha
     ege_select_one_action_set_appearance(act1, "minimal");
     g_object_weak_ref(G_OBJECT(act1), _actionFinalizedCB, this);
     g_signal_connect(G_OBJECT(act1), "changed", G_CALLBACK(_unitChangedCB), this);
-    _actionList = g_slist_append(_actionList, act1);
+    _actionList.push_back(act1);
 
     return GTK_ACTION(act1);
 }
@@ -180,9 +177,10 @@ void UnitTracker::_adjustmentFinalizedCB(gpointer data, GObject *where_the_objec
 
 void UnitTracker::_actionFinalized(GObject *where_the_object_was)
 {
-    GSList *target = g_slist_find(_actionList, where_the_object_was);
-    if (target) {
-        _actionList = g_slist_remove(_actionList, where_the_object_was);
+    EgeSelectOneAction* act = (EgeSelectOneAction*)(where_the_object_was);
+    auto it = std::find(_actionList.begin(),_actionList.end(), act);
+    if (it != _actionList.end()) {
+        _actionList.erase(it);
     } else {
         g_warning("Received a finalization callback for unknown object %p", where_the_object_was);
     }
@@ -190,9 +188,10 @@ void UnitTracker::_actionFinalized(GObject *where_the_object_was)
 
 void UnitTracker::_adjustmentFinalized(GObject *where_the_object_was)
 {
-    GSList *target = g_slist_find(_adjList, where_the_object_was);
-    if (target) {
-        _adjList = g_slist_remove(_adjList, where_the_object_was);
+    GtkAdjustment* adj = (GtkAdjustment*)(where_the_object_was);
+    auto it = std::find(_adjList.begin(),_adjList.end(), adj);
+    if (it != _adjList.end()) {
+        _adjList.erase(it);
     } else {
         g_warning("Received a finalization callback for unknown object %p", where_the_object_was);
     }
@@ -217,7 +216,7 @@ void UnitTracker::_setActive(gint active)
                 Inkscape::Util::Unit const *newUnit = unit_table.getUnit(newAbbr);
                 _activeUnit = newUnit;
 
-                if (_adjList) {
+                if (!_adjList.empty()) {
                     _fixupAdjustments(unit, newUnit);
                 }
 
@@ -230,11 +229,8 @@ void UnitTracker::_setActive(gint active)
 
         _active = active;
 
-        for ( GSList *cur = _actionList ; cur ; cur = g_slist_next(cur) ) {
-            if (IS_EGE_SELECT_ONE_ACTION(cur->data)) {
-                EgeSelectOneAction *act = EGE_SELECT_ONE_ACTION(cur->data);
-                ege_select_one_action_set_active(act, active);
-            }
+        for (auto act:_actionList) {
+            ege_select_one_action_set_active(act, active);
         }
         
         _activeUnitInitialized = true;
@@ -244,8 +240,7 @@ void UnitTracker::_setActive(gint active)
 void UnitTracker::_fixupAdjustments(Inkscape::Util::Unit const *oldUnit, Inkscape::Util::Unit const *newUnit)
 {
     _isUpdating = true;
-    for ( GSList *cur = _adjList ; cur ; cur = g_slist_next(cur) ) {
-        GtkAdjustment *adj = GTK_ADJUSTMENT(cur->data);
+    for ( auto adj : _adjList ) {
         gdouble oldVal = gtk_adjustment_get_value(adj);
         gdouble val = oldVal;
 

src/display/canvas-axonomgrid.cpp
@@ -294,8 +294,8 @@ CanvasAxonomGrid::readRepr()
         gridunit = unit_table.getUnit(value); // Display unit identifier in grid menu
     }
 
-    for (GSList *l = canvasitems; l != NULL; l = l->next) {
-        sp_canvas_item_request_update ( SP_CANVAS_ITEM(l->data) );
+    for (auto i:canvasitems) {
+        sp_canvas_item_request_update(i);
     }
     return;
 }

src/display/canvas-grid.cpp
@@ -149,7 +149,6 @@ CanvasGrid::CanvasGrid(SPNamedView * nv, Inkscape::XML::Node * in_repr, SPDocume
     }
 
     namedview = nv;
-    canvasitems = NULL;
 }
 
 CanvasGrid::~CanvasGrid()
@@ -157,11 +156,9 @@ CanvasGrid::~CanvasGrid()
     if (repr) {
         repr->removeListenerByData (this);
     }
-
-    while (canvasitems) {
-        sp_canvas_item_destroy(SP_CANVAS_ITEM(canvasitems->data));
-        canvasitems = g_slist_remove(canvasitems, canvasitems->data);
-    }
+    for (auto i:canvasitems)
+        sp_canvas_item_destroy(i);
+    canvasitems.clear();
 }
 
 const char *
@@ -276,8 +273,8 @@ CanvasGrid::createCanvasItem(SPDesktop * desktop)
 //           but share the same CanvasGrid object; that is what this function is for.
 
     // check if there is already a canvasitem on this desktop linking to this grid
-    for (GSList *l = canvasitems; l != NULL; l = l->next) {
-        if ( desktop->getGridGroup() == SP_CANVAS_GROUP(SP_CANVAS_ITEM(l->data)->parent) ) {
+    for (auto i:canvasitems) {
+        if ( desktop->getGridGroup() == SP_CANVAS_GROUP(i->parent) ) {
             return NULL;
         }
     }
@@ -287,7 +284,7 @@ CanvasGrid::createCanvasItem(SPDesktop * desktop)
     sp_canvas_item_show(SP_CANVAS_ITEM(item));
 
     g_object_ref(item);    // since we're keeping a link to this item, we need to bump up the ref count
-    canvasitems = g_slist_prepend(canvasitems, item);
+    canvasitems.push_back(item);
 
     return item;
 }
@@ -650,8 +647,8 @@ CanvasXYGrid::readRepr()
         gridunit = unit_table.getUnit(value); // Display unit identifier in grid menu
     }
 
-    for (GSList *l = canvasitems; l != NULL; l = l->next) {
-        sp_canvas_item_request_update ( SP_CANVAS_ITEM(l->data) );
+    for (auto i:canvasitems) {
+        sp_canvas_item_request_update(i);
     }
 
     return;

src/display/canvas-grid.h
@@ -112,7 +112,7 @@ protected:
 
     virtual Gtk::Widget * newSpecificWidget() = 0;
 
-    GSList * canvasitems;  // list of created canvasitems
+    std::vector<SPCanvasItem*> canvasitems;  // list of created canvasitems
 
     SPNamedView * namedview;
 

src/display/curve.cpp
@@ -38,6 +38,15 @@ SPCurve::SPCurve(Geom::PathVector const& pathv)
     _pathv(pathv)
 {}
 
+//concat constructor
+SPCurve::SPCurve(std::list<SPCurve *> const& l) : _refcount(1)
+{
+    for (auto c:l) {
+        _pathv.insert( _pathv.end(), c->get_pathvector().begin(), c->get_pathvector().end() );
+    }
+}
+
+
 SPCurve *
 SPCurve::new_from_rect(Geom::Rect const &rect, bool all_four_sides)
 {
@@ -134,32 +143,24 @@ SPCurve::copy() const
  * Return new curve that is the concatenation of all curves in list.
  */
 SPCurve *
-SPCurve::concat(GSList const *list)
-{
-    SPCurve *new_curve = new SPCurve();
-
-    for (GSList const *l = list; l != NULL; l = l->next) {
-        SPCurve *c = static_cast<SPCurve *>(l->data);
-        new_curve->_pathv.insert( new_curve->_pathv.end(), c->get_pathvector().begin(), c->get_pathvector().end() );
-    }
-
-    return new_curve;
-}
+SPCurve::concat(std::list<SPCurve *> l){
+    return new SPCurve(l);
+};
 
 /**
  * Returns a list of new curves corresponding to the subpaths in \a curve.
  * 2geomified
  */
-GSList *
+std::list<SPCurve *>
 SPCurve::split() const
 {
-    GSList *l = NULL;
+    std::list<SPCurve *> l;
 
     for (Geom::PathVector::const_iterator path_it = _pathv.begin(); path_it != _pathv.end(); ++path_it) {
         Geom::PathVector newpathv;
         newpathv.push_back(*path_it);
         SPCurve * newcurve = new SPCurve(newpathv);
-        l = g_slist_prepend(l, newcurve);
+        l.push_back(newcurve);
     }
 
     return l;

src/display/curve.h
@@ -16,8 +16,7 @@
 #include <2geom/pathvector.h>
 #include <cstddef>
 #include <boost/optional.hpp>
-
-extern "C" { typedef struct _GSList GSList; }
+#include <list>
 
 /**
  * Wrapper around a Geom::PathVector object.
@@ -27,6 +26,7 @@ public:
     /* Constructors */
     explicit SPCurve();
     explicit SPCurve(Geom::PathVector const& pathv);
+    explicit SPCurve(std::list<SPCurve *> const& pathv);
     static SPCurve * new_from_rect(Geom::Rect const &rect, bool all_four_sides = false);
 
     virtual ~SPCurve();
@@ -78,8 +78,8 @@ public:
     SPCurve * append_continuous(SPCurve const *c1, double tolerance);
     SPCurve * create_reverse() const;
 
-    GSList * split() const;
-    static SPCurve * concat(GSList const *list);
+    std::list<SPCurve *> split() const;
+    static SPCurve * concat(std::list<SPCurve *> l);
 
 protected:
     size_t _refcount;

src/extension/extension.cpp
@@ -64,7 +64,6 @@ Extension::Extension (Inkscape::XML::Node * in_repr, Implementation::Implementat
     id = NULL;
     name = NULL;
     _state = STATE_UNLOADED;
-    parameters = NULL;
 
     if (in_imp == NULL) {
         imp = new Implementation::Implementation();
@@ -97,7 +96,7 @@ Extension::Extension (Inkscape::XML::Node * in_repr, Implementation::Implementat
                 Parameter * param;
                 param = Parameter::make(child_repr, this);
                 if (param != NULL)
-                    parameters = g_slist_append(parameters, param);
+                    parameters.push_back(param);
             } /* param || _param */
             if (!strcmp(chname, "dependency")) {
                 _deps.push_back(new Dependency(child_repr));
@@ -146,12 +145,9 @@ Extension::~Extension (void)
     /** \todo Need to do parameters here */
 
     // delete parameters:
-    for (GSList * list = parameters; list != NULL; list = g_slist_next(list)) {
-        Parameter * param = reinterpret_cast<Parameter *>(list->data);
+    for (auto param:parameters) {
         delete param;
     }
-    g_slist_free(parameters);
-
 
     for (unsigned int i = 0 ; i < _deps.size(); i++) {
         delete _deps[i];
@@ -402,14 +398,11 @@ Parameter *Extension::get_param(gchar const *name)
     if (name == NULL) {
         throw Extension::param_not_exist();
     }
-    if (this->parameters == NULL) {
-        // the list of parameters is empty
+    if (this->parameters.empty()) {
         throw Extension::param_not_exist();
     }
 
-    for (GSList * list = this->parameters; list != NULL; list =
-g_slist_next(list)) {
-        Parameter * param = static_cast<Parameter*>(list->data);
+    for( auto param:this->parameters) {
         if (!strcmp(param->name(), name)) {
             return param;
         } else {
@@ -730,8 +723,7 @@ Extension::autogui (SPDocument * doc, Inkscape::XML::Node * node, sigc::signal<v
     agui->set_spacing(Parameter::GUI_BOX_SPACING);
 
     //go through the list of parameters to see if there are any non-hidden ones
-    for (GSList * list = parameters; list != NULL; list = g_slist_next(list)) {
-        Parameter * param = reinterpret_cast<Parameter *>(list->data);
+    for (auto param:parameters) {
         if (param->get_hidden()) continue; //Ignore hidden parameters
         Gtk::Widget * widg = param->get_widget(doc, node, changeSignal);
         gchar const * tip = param->get_tooltip();
@@ -751,8 +743,7 @@ Extension::autogui (SPDocument * doc, Inkscape::XML::Node * node, sigc::signal<v
 void
 Extension::paramListString (std::list <std::string> &retlist)
 {
-    for (GSList * list = parameters; list != NULL; list = g_slist_next(list)) {
-        Parameter * param = reinterpret_cast<Parameter *>(list->data);
+    for(auto param:parameters) {
         param->string(retlist);
     }
 
@@ -835,8 +826,7 @@ Extension::get_params_widget(void)
 unsigned int Extension::param_visible_count ( )
 {
     unsigned int _visible_count = 0;
-    for (GSList * list = parameters; list != NULL; list = g_slist_next(list)) {
-        Parameter * param = reinterpret_cast<Parameter *>(list->data);
+    for (auto param:parameters) {
         if (!param->get_hidden()) _visible_count++;
     }
     return _visible_count;

src/extension/extension.h
@@ -133,7 +133,7 @@ public:
 
 /* Parameter Stuff */
 private:
-    GSList * parameters; /**< A table to store the parameters for this extension.
+    std::vector<Parameter *> parameters; /**< A table to store the parameters for this extension.
                               This only gets created if there are parameters in this
                               extension */
 
@@ -141,8 +141,7 @@ public:
     /** \brief  A function to get the number of parameters that
                 the extension has.
         \return The number of parameters. */
-    unsigned int param_count ( ) { return parameters == NULL ? 0 :
-                                              g_slist_length(parameters); };
+    unsigned int param_count ( ) { return parameters.size(); };
     /** \brief  A function to get the number of parameters that
                 are visible to the user that the extension has.
         \return The number of visible parameters.

commit a5857216ddf10c103d04d797c839455195f7793e

Relink GtkAdjustments that use same UnitTracker. Link broken by commit e0957537 (removing GSList).

src/ui/widget/unit-tracker.cpp
@@ -106,7 +106,7 @@ void UnitTracker::setActiveUnitByAbbr(gchar const *abbr)
 
 void UnitTracker::addAdjustment(GtkAdjustment *adj)
 {
-    if (std::find(_adjList.begin(),_adjList.end(),adj)!=_adjList.end()) {
+    if (std::find(_adjList.begin(),_adjList.end(),adj) == _adjList.end()) {
         g_object_weak_ref(G_OBJECT(adj), _adjustmentFinalizedCB, this);
         _adjList.push_back(adj);
     }