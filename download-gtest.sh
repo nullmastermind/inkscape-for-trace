0566f18c8d7a92870847e370b2b8e080b8cc16ee
Shlomi Fish <shlomif@shlomifish.org>
2018-10-13 15:39:43 +0300

    update googletest and refactor the script.

--- download-gtest.sh
+++ download-gtest.sh
@@ -5,13 +5,18 @@ fi
 (
     cd gtest
     if ! test -e gtest ; then
-        wget -O 'googletest-release-1.7.0.tar.gz' https://github.com/google/googletest/archive/release-1.7.0.tar.gz
-        tar -xvf 'googletest-release-1.7.0.tar.gz'
-        mv googletest-release-1.7.0 gtest
+        gtest_ver=1.8.1
+        wget -O "googletest-release-$gtest_ver.tar.gz" https://github.com/google/googletest/archive/release-$gtest_ver.tar.gz
+        tar -xvf "googletest-release-$gtest_ver.tar.gz"
+        mv googletest-release-$gtest_ver gtest
     fi
-    if ! test -e gmock-1.7.0 ; then
-        wget -O 'googlemock-release-1.7.0.tar.gz' https://github.com/google/googlemock/archive/release-1.7.0.tar.gz
-        tar -xvf 'googlemock-release-1.7.0.tar.gz'
-        mv googlemock-release-1.7.0 gmock-1.7.0
+    if false
+    then
+        mock_ver=1.7.0
+        if ! test -e gmock-$gmock_ver ; then
+            wget -O "googlemock-release-$gmock_ver.tar.gz" https://github.com/google/googlemock/archive/release-$gmock_ver.tar.gz
+            tar -xvf "googlemock-release-$gmock_ver.tar.gz"
+            mv googlemock-release-$gmock_ver gmock-$gmock_ver
+        fi
     fi
 )
