6a62493df0dbc9c6dba92ca014942c5dee863baa
Shlomi Fish <shlomif@shlomifish.org>
2018-10-13 17:24:21 +0300

    remove old code.

--- download-gtest.sh
+++ download-gtest.sh
@@ -10,13 +10,4 @@ fi
         tar -xvf "googletest-release-$gtest_ver.tar.gz"
         mv googletest-release-$gtest_ver gtest
     fi
-    if false
-    then
-        mock_ver=1.7.0
-        if ! test -e gmock-$gmock_ver ; then
-            wget -O "googlemock-release-$gmock_ver.tar.gz" https://github.com/google/googlemock/archive/release-$gmock_ver.tar.gz
-            tar -xvf "googlemock-release-$gmock_ver.tar.gz"
-            mv googlemock-release-$gmock_ver gmock-$gmock_ver
-        fi
-    fi
 )
