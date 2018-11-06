0007be630b7c9b2ffc86d358a9d0fb09a31f37a0
Tavmjong Bah <tavmjong@free.fr>
2018-11-06 14:23:31 +0100

    Use C++11 raw string for embeded SVG.

--- src/widgets/stroke-marker-selector.cpp
+++ src/widgets/stroke-marker-selector.cpp
@@ -555,31 +555,33 @@ gboolean MarkerComboBox::separator_cb (GtkTreeModel *model, GtkTreeIter *iter, g
  */
 SPDocument *MarkerComboBox::ink_markers_preview_doc ()
 {
-gchar const *buffer = "<svg xmlns=\"http://www.w3.org/2000/svg\" xmlns:sodipodi=\"http://sodipodi.sourceforge.net/DTD/sodipodi-0.dtd\" xmlns:inkscape=\"http://www.inkscape.org/namespaces/inkscape\" xmlns:xlink=\"http://www.w3.org/1999/xlink\" id=\"MarkerSample\">"
-"  <defs id=\"defs\" />"
+gchar const *buffer = R"A(
+    <svg xmlns="http://www.w3.org/2000/svg"
+         xmlns:xlink="http://www.w3.org/1999/xlink"
+         id="MarkerSample">
 
-"  <g id=\"marker-start\">"
-"    <path style=\"fill:gray;stroke:darkgray;stroke-width:1.7;marker-start:url(#sample);marker-mid:none;marker-end:none\""
-"       d=\"M 12.5,13 L 25,13\" id=\"path1\" />"
-"    <rect style=\"fill:none;stroke:none\" id=\"rect2\""
-"       width=\"25\" height=\"25\" x=\"0\" y=\"0\" />"
-"  </g>"
+    <defs id="defs"/>
 
-"  <g id=\"marker-mid\">"
-"    <path style=\"fill:gray;stroke:darkgray;stroke-width:1.7;marker-start:none;marker-mid:url(#sample);marker-end:none\""
-"       d=\"M 0,113 L 12.5,113 L 25,113\" id=\"path11\" />"
-"    <rect style=\"fill:none;stroke:none\" id=\"rect22\""
-"       width=\"25\" height=\"25\" x=\"0\" y=\"100\" />"
-"  </g>"
+    <g id="marker-start">
+      <path style="fill:gray;stroke:darkgray;stroke-width:1.7;marker-start:url(#sample)"
+       d="M 12.5,13 L 25,13"/>
+      <rect x="0" y="0" width="25" height="25" style="fill:none;stroke:none"/>
+    </g>
 
-"  <g id=\"marker-end\">"
-"    <path style=\"fill:gray;stroke:darkgray;stroke-width:1.7;marker-start:none;marker-mid:none;marker-end:url(#sample)\""
-"       d=\"M 0,213 L 12.5,213\" id=\"path111\" />"
-"    <rect style=\"fill:none;stroke:none\" id=\"rect222\""
-"       width=\"25\" height=\"25\" x=\"0\" y=\"200\" />"
-"  </g>"
+    <g id="marker-mid">
+      <path style="fill:gray;stroke:darkgray;stroke-width:1.7;marker-mid:url(#sample)"
+       d="M 0,113 L 12.5,113 L 25,113"/>
+      <rect x="0" y="100" width="25" height="25" style="fill:none;stroke:none"/>
+    </g>
 
-"</svg>";
+    <g id="marker-end">
+      <path style="fill:gray;stroke:darkgray;stroke-width:1.7;marker-end:url(#sample)"
+       d="M 0,213 L 12.5,213"/>
+      <rect x="0" y="200" width="25" height="25" style="fill:none;stroke:none"/>
+    </g>
+
+  </svg>
+)A";
 
     return SPDocument::createNewDocFromMem (buffer, strlen(buffer), FALSE);
 }
