22ceac00bfe22a63e61af6ea1bbd22e7c7bbe715
Jabier Arraiza <jabier.arraiza@marker.es>
2019-04-26 23:20:02 +0200

    Merge from master

--- src/ui/dialog/selectordialog.cpp
+++ src/ui/dialog/selectordialog.cpp
@@ -503,7 +503,7 @@ void SelectorDialog::_readStyleElement()
                 Gtk::TreeModel::Row childrow = *(_store->append(row->children()));
                 childrow[_mColumns._colSelector] = "#" + Glib::ustring(obj->getId());
                 childrow[_mColumns._colExpand] = false;
-                childrow[_mColumns._colType] = colType == UNHANDLED?UNHANDLED:OBJECT;;
+                childrow[_mColumns._colType] = colType == UNHANDLED ? UNHANDLED : OBJECT;;
                 childrow[_mColumns._colObj] = std::vector<SPObject *>(1, obj);
                 childrow[_mColumns._colProperties] = ""; // Unused
                 childrow[_mColumns._colVisible] = true; // Unused
