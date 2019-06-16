nullmastermind/inkscape-for-trace

commit de137937630524727b287fb2e03ec470554683a8

Some styling tweaks

src/ui/dialog/livepatheffect-add.cpp
@@ -119,17 +119,22 @@ LivePathEffectAdd::LivePathEffectAdd()
         builder_effect->get_widget("LPEIcon", LPEIcon);
         LPEIcon->set_from_icon_name(converter.get_icon(data->id), Gtk::BuiltinIconSize(Gtk::ICON_SIZE_DIALOG));
         Gtk::EventBox *LPESelectorEffectEventInfo;
-        //LPESelectorEffectEventFav->add_events(Gdk::BUTTON_PRESS_MASK);  
         builder_effect->get_widget("LPESelectorEffectEventInfo", LPESelectorEffectEventInfo);
         LPESelectorEffectEventInfo->signal_button_press_event().connect(sigc::bind<Glib::RefPtr<Gtk::Builder> >(sigc::mem_fun(*this, &LivePathEffectAdd::pop_description), builder_effect));
         Gtk::EventBox *LPESelectorEffectEventFav;
         builder_effect->get_widget("LPESelectorEffectEventFav", LPESelectorEffectEventFav);
-       // LPESelectorEffectEventFav->add_events(Gdk::BUTTON_PRESS_MASK);    
+        if (sp_has_fav(LPEName->get_text())){
+            Gtk::Image *fav = dynamic_cast<Gtk::Image *>(LPESelectorEffectEventFav->get_child());
+            fav->set_from_icon_name("draw-star", Gtk::IconSize(25));
+        }
         Gtk::EventBox *LPESelectorEffectEventFavTop;
         builder_effect->get_widget("LPESelectorEffectEventFavTop", LPESelectorEffectEventFavTop);
-        //LPESelectorEffectFavTop->add_events(Gdk::BUTTON_PRESS_MASK);    
-        LPESelectorEffectEventFav   ->signal_button_press_event().connect(sigc::bind<Glib::RefPtr<Gtk::Builder> >(sigc::mem_fun(*this, &LivePathEffectAdd::fav_toggler), builder_effect));
+        LPESelectorEffectEventFav->signal_button_press_event().connect(sigc::bind<Glib::RefPtr<Gtk::Builder> >(sigc::mem_fun(*this, &LivePathEffectAdd::fav_toggler), builder_effect));
         LPESelectorEffectEventFavTop->signal_button_press_event().connect(sigc::bind<Glib::RefPtr<Gtk::Builder> >(sigc::mem_fun(*this, &LivePathEffectAdd::fav_toggler), builder_effect));
+        Gtk::ButtonBox *LPESelectorButtonBox;
+        builder_effect->get_widget("LPESelectorButtonBox", LPESelectorButtonBox);
+        LPESelectorButtonBox->signal_enter_notify_event().connect(sigc::bind<GtkWidget *>(sigc::mem_fun(*this, &LivePathEffectAdd::mouseover), GTK_WIDGET(LPESelectorEffect->gobj())));
+        LPESelectorButtonBox->signal_leave_notify_event().connect(sigc::bind<GtkWidget *>(sigc::mem_fun(*this, &LivePathEffectAdd::mouseout), GTK_WIDGET(LPESelectorEffect->gobj())));
         LPESelectorEffect->signal_enter_notify_event().connect(sigc::bind<GtkWidget *>(sigc::mem_fun(*this, &LivePathEffectAdd::mouseover), GTK_WIDGET(LPESelectorEffect->gobj())));
         LPESelectorEffect->signal_leave_notify_event().connect(sigc::bind<GtkWidget *>(sigc::mem_fun(*this, &LivePathEffectAdd::mouseout), GTK_WIDGET(LPESelectorEffect->gobj())));
         _LPESelectorFlowBox->insert(*LPESelectorEffect, i);
@@ -212,6 +217,7 @@ bool LivePathEffectAdd::fav_toggler(GdkEventButton* evt, Glib::RefPtr<Gtk::Build
             //favimg->set_from_icon_name("draw-star-outline", Gtk::IconSize(30));
             LPESelectorEffectEventFavTop->set_visible(false);
             LPESelectorEffectEventFavTop->hide();
+            LPESelectorEffectFav->set_from_icon_name("draw-star-outline", Gtk::IconSize(25));
             sp_remove_fav(LPEName->get_text());
             LPESelectorEffect->get_parent()->get_style_context()->remove_class("lpefav");
             _LPESelectorFlowBox->invalidate_sort();
@@ -219,6 +225,7 @@ bool LivePathEffectAdd::fav_toggler(GdkEventButton* evt, Glib::RefPtr<Gtk::Build
             //favimg->set_from_icon_name("draw-star", Gtk::IconSize(30));
             LPESelectorEffectEventFavTop->set_visible(true);
             LPESelectorEffectEventFavTop->show();
+            LPESelectorEffectFav->set_from_icon_name("draw-star", Gtk::IconSize(25));
             sp_add_fav(LPEName->get_text());
             LPESelectorEffect->get_parent()->get_style_context()->add_class("lpefav");
             Glib::RefPtr< Gtk::Adjustment > vadjust = _LPEScrolled->get_vadjustment();
@@ -231,6 +238,7 @@ bool LivePathEffectAdd::fav_toggler(GdkEventButton* evt, Glib::RefPtr<Gtk::Build
 
 bool LivePathEffectAdd::on_filter(Gtk::FlowBoxChild *child)
 {
+    child->set_valign(Gtk::ALIGN_START);
     Gtk::EventBox *eventbox = dynamic_cast<Gtk::EventBox *>(child->get_child());
     if (eventbox) {
         Gtk::Box *box = dynamic_cast<Gtk::Box *>(eventbox->get_child());

share/ui/dialog-livepatheffect-add-effect.ui
@@ -118,7 +118,7 @@
           </packing>
         </child>
         <child>
-          <object class="GtkButtonBox">
+          <object class="GtkButtonBox" id="LPESelectorButtonBox">
             <property name="visible">True</property>
             <property name="can_focus">False</property>
             <property name="halign">center</property>
@@ -139,7 +139,7 @@
                     <property name="can_focus">False</property>
                     <property name="halign">end</property>
                     <property name="valign">start</property>
-                    <property name="pixel_size">20</property>
+                    <property name="pixel_size">23</property>
                     <property name="icon_name">infopop-outline</property>
                     <property name="icon_size">2</property>
                     <style>
@@ -170,7 +170,7 @@
                     <property name="can_focus">False</property>
                     <property name="halign">center</property>
                     <property name="valign">start</property>
-                    <property name="pixel_size">20</property>
+                    <property name="pixel_size">23</property>
                     <property name="icon_name">draw-star-outline</property>
                     <property name="icon_size">2</property>
                     <style>
@@ -200,7 +200,7 @@
                     <property name="can_focus">False</property>
                     <property name="halign">start</property>
                     <property name="valign">start</property>
-                    <property name="pixel_size">20</property>
+                    <property name="pixel_size">23</property>
                     <property name="icon_name">on</property>
                     <property name="icon_size">2</property>
                     <style>

share/ui/dialog-livepatheffect-add.ui
@@ -160,10 +160,10 @@
                         <property name="visible">True</property>
                         <property name="can_focus">False</property>
                         <property name="no_show_all">True</property>
-                        <property name="column_spacing">2</property>
-                        <property name="row_spacing">2</property>
-                        <property name="min_children_per_line">3</property>
-                        <property name="max_children_per_line">20</property>
+                        <property name="column_spacing">1</property>
+                        <property name="row_spacing">1</property>
+                        <property name="min_children_per_line">1</property>
+                        <property name="max_children_per_line">300</property>
                       </object>
                     </child>
                   </object>

share/ui/style.css
@@ -219,14 +219,14 @@ spinbutton undershoot {
     border-right-width:0;
 }
 
-    #lpeflow GtkFlowBoxChild,
-    #lpeflow flowboxchild{
-        border-width:0px;
-        border-radius:6px;
-        margin:10px;
-        margin-bottom:30px;
-        padding-bottom:10px;
-    }
+#lpeflow GtkFlowBoxChild,
+#lpeflow flowboxchild{
+    border-width:0px;
+    border-radius:6px;
+    margin:10px;
+    margin-bottom:30px;
+    padding-bottom:10px;
+}
 
 #lpeflow GtkFlowBoxChild.lpeactive,
 #lpeflow flowboxchild.lpeactive{
@@ -245,7 +245,7 @@ spinbutton undershoot {
     border-radius:6px;
     margin:10px;
     padding:10px;
-    margin-bottom:20px;
+    margin-bottom:23px;
     padding-bottom:10px;
 }
   
@@ -254,6 +254,9 @@ spinbutton undershoot {
     margin-bottom:0px;
     padding-bottom:10px;
     outline:none;
+}
+.lpeactive #LPESelectorEffectFavTop{
+    opacity:0;
 }
 .lpefav{
     background-color: @theme_base_color;

commit 0a4ab9a3f12609a955cd02ea4b22aa901fdd1eb1

Remove deprecated usage of custom Gtk::IconSize
Custom Gtk::IconSize values have been deprecated since GTK 3.10.

Also fix incorrect usage of pixel values to the Gtk::IconSize()
constructor (introduced in de13793763), these are in fact meant to be
enum values.

Refs: https://gitlab.com/inkscape/inkscape/commit/de137937630524727b287fb2e03ec470554683a8

src/ui/dialog/livepatheffect-add.cpp
@@ -141,7 +141,7 @@ LivePathEffectAdd::LivePathEffectAdd()
         LPEExperimentalToggle->set_active(active);
         Gtk::Image *LPEIcon;
         builder_effect->get_widget("LPEIcon", LPEIcon);
-        LPEIcon->set_from_icon_name(converter.get_icon(data->id), Gtk::BuiltinIconSize(Gtk::ICON_SIZE_DIALOG));
+        LPEIcon->set_from_icon_name(converter.get_icon(data->id), Gtk::IconSize(Gtk::ICON_SIZE_DIALOG));
         Gtk::EventBox *LPESelectorEffectEventInfo;
         builder_effect->get_widget("LPESelectorEffectEventInfo", LPESelectorEffectEventInfo);
         LPESelectorEffectEventInfo->signal_enter_notify_event().connect(sigc::bind<Glib::RefPtr<Gtk::Builder>>(
@@ -150,7 +150,7 @@ LivePathEffectAdd::LivePathEffectAdd()
         builder_effect->get_widget("LPESelectorEffectEventFav", LPESelectorEffectEventFav);
         if (sp_has_fav(LPEName->get_text())) {
             Gtk::Image *fav = dynamic_cast<Gtk::Image *>(LPESelectorEffectEventFav->get_child());
-            fav->set_from_icon_name("draw-star", Gtk::IconSize(25));
+            fav->set_from_icon_name("draw-star", Gtk::IconSize(Gtk::ICON_SIZE_SMALL_TOOLBAR));
         }
         Gtk::EventBox *LPESelectorEffectEventFavTop;
         builder_effect->get_widget("LPESelectorEffectEventFavTop", LPESelectorEffectEventFavTop);
@@ -271,7 +271,7 @@ bool LivePathEffectAdd::pop_description(GdkEventCrossing *evt, Glib::RefPtr<Gtk:
 
     Gtk::Image *LPESelectorEffectInfoIcon;
     _builder->get_widget("LPESelectorEffectInfoIcon", LPESelectorEffectInfoIcon);
-    LPESelectorEffectInfoIcon->set_from_icon_name(LPEIcon->get_icon_name(), Gtk::IconSize(60));
+    LPESelectorEffectInfoIcon->set_from_icon_name(LPEIcon->get_icon_name(), Gtk::IconSize(Gtk::ICON_SIZE_DIALOG));
 
     Gtk::Label *LPESelectorEffectInfoName;
     _builder->get_widget("LPESelectorEffectInfoName", LPESelectorEffectInfoName);
@@ -306,7 +306,7 @@ bool LivePathEffectAdd::fav_toggler(GdkEventButton *evt, Glib::RefPtr<Gtk::Build
         if (sp_has_fav(LPEName->get_text())) {
             LPESelectorEffectEventFavTop->set_visible(false);
             LPESelectorEffectEventFavTop->hide();
-            LPESelectorEffectFav->set_from_icon_name("draw-star-outline", Gtk::IconSize(25));
+            LPESelectorEffectFav->set_from_icon_name("draw-star-outline", Gtk::IconSize(Gtk::ICON_SIZE_SMALL_TOOLBAR));
             sp_remove_fav(LPEName->get_text());
             LPESelectorEffect->get_parent()->get_style_context()->remove_class("lpefav");
             LPESelectorEffect->get_parent()->get_style_context()->add_class("lpenormal");
@@ -317,7 +317,7 @@ bool LivePathEffectAdd::fav_toggler(GdkEventButton *evt, Glib::RefPtr<Gtk::Build
         } else {
             LPESelectorEffectEventFavTop->set_visible(true);
             LPESelectorEffectEventFavTop->show();
-            LPESelectorEffectFav->set_from_icon_name("draw-star", Gtk::IconSize(25));
+            LPESelectorEffectFav->set_from_icon_name("draw-star", Gtk::IconSize(Gtk::ICON_SIZE_SMALL_TOOLBAR));
             sp_add_fav(LPEName->get_text());
             LPESelectorEffect->get_parent()->get_style_context()->add_class("lpefav");
             LPESelectorEffect->get_parent()->get_style_context()->remove_class("lpenormal");
@@ -333,14 +333,15 @@ bool LivePathEffectAdd::show_fav_toggler(GdkEventButton *evt)
     Gtk::Image *favimage = dynamic_cast<Gtk::Image *>(_LPESelectorEffectEventFavShow->get_child());
     if (favimage) {
         if (_showfavs) {
-            favimage->set_from_icon_name("draw-star", Gtk::IconSize(favimage->get_pixel_size()));
+            favimage->set_from_icon_name("draw-star", Gtk::IconSize(Gtk::ICON_SIZE_SMALL_TOOLBAR));
         } else {
-            favimage->set_from_icon_name("draw-star-outline", Gtk::IconSize(favimage->get_pixel_size()));
+            favimage->set_from_icon_name("draw-star-outline", Gtk::IconSize(Gtk::ICON_SIZE_SMALL_TOOLBAR));
         }
     }
     reload_effect_list();
     return true;
 }
+
 bool LivePathEffectAdd::apply(GdkEventButton *evt, Glib::RefPtr<Gtk::Builder> builder_effect,
                               const LivePathEffect::EnumEffectData<LivePathEffect::EffectType> *to_add)
 {

src/ui/dialog/symbols.cpp
@@ -202,37 +202,38 @@ SymbolsDialog::SymbolsDialog( gchar const* prefsPath ) :
   overlay->get_style_context()->add_class("colorbright");
   overlay->get_style_context()->add_class("backgroundbright");
   scroller->set_size_request(100, 250);
-  table->attach(*Gtk::manage(overlay),0,row,2,1);
+  table->attach(*Gtk::manage(overlay), 0, row, 2, 1);
 
   /*************************Overlays******************************/
   overlay_opacity = new Gtk::Image();
-  overlay_opacity->set_halign(Gtk::ALIGN_START );
-  overlay_opacity->set_valign(Gtk::ALIGN_START );
-  //No results
-  iconsize = Gtk::IconSize().from_name(Glib::ustring("ICON_SIZE_DIALOG_EXTRA"));
-  if (!iconsize) {
-      iconsize = Gtk::IconSize().register_new(Glib::ustring("ICON_SIZE_DIALOG_EXTRA"), 110, 110);
-  }
-  overlay_icon = sp_get_icon_image("searching", iconsize);
+  overlay_opacity->set_halign(Gtk::ALIGN_START);
+  overlay_opacity->set_valign(Gtk::ALIGN_START);
+
+  // No results
+  overlay_icon = sp_get_icon_image("searching", Gtk::ICON_SIZE_DIALOG);
+  overlay_icon->set_pixel_size(110);
   overlay_icon->get_style_context()->add_class("iconsymbolic");
-  overlay_icon->set_halign(Gtk::ALIGN_CENTER );
-  overlay_icon->set_valign(Gtk::ALIGN_START );
+  overlay_icon->set_halign(Gtk::ALIGN_CENTER);
+  overlay_icon->set_valign(Gtk::ALIGN_START);
   overlay_icon->set_margin_top(45);
+
   overlay_title = new Gtk::Label();
   overlay_title->set_halign(Gtk::ALIGN_CENTER );
   overlay_title->set_valign(Gtk::ALIGN_START );
   overlay_title->set_justify(Gtk::JUSTIFY_CENTER);
   overlay_title->set_margin_top(155);
+
   overlay_desc = new Gtk::Label();
-  overlay_desc->set_halign(Gtk::ALIGN_CENTER );
-  overlay_desc->set_valign(Gtk::ALIGN_START );
+  overlay_desc->set_halign(Gtk::ALIGN_CENTER);
+  overlay_desc->set_valign(Gtk::ALIGN_START);
   overlay_desc->set_margin_top(180);
   overlay_desc->set_justify(Gtk::JUSTIFY_CENTER);
-  overlay->add_overlay(* overlay_opacity);
-  overlay->add_overlay(* overlay_icon);
-  overlay->add_overlay(* overlay_title);
-  overlay->add_overlay(* overlay_desc);
-  
+
+  overlay->add_overlay(*overlay_opacity);
+  overlay->add_overlay(*overlay_icon);
+  overlay->add_overlay(*overlay_title);
+  overlay->add_overlay(*overlay_desc);
+
   previous_height = 0;
   ++row;
 