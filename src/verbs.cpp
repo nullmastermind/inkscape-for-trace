78185938095d42a627407edaa2a41ab663c82b4a
Jabier Arraiza <jabier.arraiza@marker.es>
2019-07-28 02:26:21 +0200

    Fix coding style

--- src/verbs.cpp
+++ src/verbs.cpp
@@ -3060,8 +3060,7 @@ Verb *Verb::_base_verbs[] = {
     new ZoomVerb(SP_VERB_VIEW_TOGGLE_SPLIT, "ViewSplitModeToggle", N_("_Split View Mode"),
                  N_("Split canvas in 2 to show outline"), nullptr),
 
-    new ZoomVerb(SP_VERB_VIEW_TOGGLE_XRAY, "ViewXRayToggle", N_("_XRay Mode"), N_("XRay around cursor"),
-                 nullptr),
+    new ZoomVerb(SP_VERB_VIEW_TOGGLE_XRAY, "ViewXRayToggle", N_("_XRay Mode"), N_("XRay around cursor"), nullptr),
 
     new ZoomVerb(SP_VERB_VIEW_CMS_TOGGLE, "ViewCmsToggle", N_("Color-Managed View"),
                  N_("Toggle color-managed display for this document window"), INKSCAPE_ICON("color-management")),
