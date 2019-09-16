d2ef70954e92b296fd8a03a6d26f4bc98e4b2369
Jabier Arraiza <jabier.arraiza@marker.es>
2019-09-16 21:35:57 +0200

    Fix operands in LPE bool

--- src/live_effects/lpe-bool.cpp
+++ src/live_effects/lpe-bool.cpp
@@ -373,7 +373,7 @@ void LPEBool::doEffect(SPCurve *curve)
             operand->setHidden(false);
         }
         bool_op_ex op = bool_operation.get_value();
-        bool swap =  swap_operands.get_value();
+        bool swap =  !(swap_operands.get_value());
 
         // operand->set_transform(i2anc_affine(sp_lpe_item, sp_lpe_item->parent));
         Geom::Affine current = sp_item_transform_repr(sp_lpe_item);
