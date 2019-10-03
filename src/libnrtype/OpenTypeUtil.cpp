573ad5a1fc6195119c159c127b66cd64d98d4019
Tavmjong Bah <tavmjong@free.fr>
2019-10-03 14:23:37 +0200

    Fix comparison between char and value outside range for char in check for gzipped data.

--- src/libnrtype/OpenTypeUtil.cpp
+++ src/libnrtype/OpenTypeUtil.cpp
@@ -382,7 +382,8 @@ void readOpenTypeSVGTable(const FT_Face ft_face,
 
         std::string svg;
 
-        if (lengthGlyph > 1 && data[offsetGlyph] == 0x1f && data[offsetGlyph + 1] == 0x8b) {
+        // static cast is needed as hb_blob_get_length returns char but we are comparing to a value greater than allowed by char.
+        if (lengthGlyph > 1 && data[offsetGlyph] == 0x1f && static_cast<unsigned char>(data[offsetGlyph + 1]) == 0x8b) {
             // Glyph is gzipped
 
             std::vector<unsigned char> buffer;
