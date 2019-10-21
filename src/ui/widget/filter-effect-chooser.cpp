966430dfc94b1552d3dbf6168caeddcf30d0f21c
Jabier Arraiza <jabier.arraiza@marker.es>
2019-10-21 02:15:00 +0200

    Remove isolate check. Need to find a way to isolate all containers

--- src/ui/widget/filter-effect-chooser.cpp
+++ src/ui/widget/filter-effect-chooser.cpp
@@ -74,13 +74,21 @@ SimpleFilterModifier::SimpleFilterModifier(int flags)
         _lb_blend.set_mnemonic_widget(_blend);
         _hb_blend.pack_start(_lb_blend, false, false, 5);
         _hb_blend.pack_start(_blend, false, false, 5);
-        if (flags & ISOLATION) {
+        /*
+        * For best fit inkscape-browsers with no GUI to isolation we need all groups, 
+        * clones and symbols with isolation == isolate to not show to the user of
+        * Inkscape a "strange" behabiour from the designer point of view. 
+        * Is strange because only happends when object not has clip, mask, 
+        * filter, blending or opacity .
+        * Anyway the feature is a no-gui feature and render as spected.
+        /*
+        /* if (flags & ISOLATION) {
             _isolation.property_active() = false;
             _hb_blend.pack_start(_isolation, false, false, 5);
             _hb_blend.pack_start(_lb_isolation, false, false, 5);
             _isolation.set_tooltip_text("Don't blend childrens with objects behind");
             _lb_isolation.set_tooltip_text("Don't blend childrens with objects behind");
-        }
+        } */
         _hb_blend.pack_start(_lb_blend, false, false, 5);
         _hb_blend.pack_start(_blend, false, false, 5);
         Gtk::Separator *separator = Gtk::manage(new Gtk::Separator());  
