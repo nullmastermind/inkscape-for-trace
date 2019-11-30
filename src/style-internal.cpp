a5d079675f1554a271b0b28b83d16cee906d4a4a
Thomas Holder <thomas@thomas-holder.de>
2019-11-30 09:29:44 +0100

    fix object-style-test (failed on macOS)

--- src/style-internal.cpp
+++ src/style-internal.cpp
@@ -3131,6 +3131,7 @@ template class SPIEnum<SPCSSFontStyle>;
 template class SPIEnum<SPCSSFontVariant>;
 template class SPIEnum<SPCSSFontVariantPosition>;
 template class SPIEnum<SPCSSFontVariantCaps>;
+template class SPIEnum<SPCSSFontWeight>;
 template class SPIEnum<uint_least16_t>;
 template class SPIEnum<uint_least8_t>;
 
