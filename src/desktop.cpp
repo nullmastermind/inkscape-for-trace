5eb8591f679fc81e8e4e01a37d1dac7feff68f97
Tavmjong Bah <tavmjong@free.fr>
2019-12-06 12:57:06 +0000

    Skip restoring InkscapePreferences dialog under Wayland to avoid crash. Fix for https://gitlab.com/inkscape/inkscape/issues/454

--- src/desktop.cpp
+++ src/desktop.cpp
@@ -2145,8 +2145,21 @@ SPDesktop::show_dialogs()
 
     for (std::map<Glib::ustring, Glib::ustring>::const_iterator iter = mapVerbPreference.begin(); iter != mapVerbPreference.end(); ++iter) {
         Glib::ustring pref = iter->second;
+
         int visible = prefs->getInt(pref + "/visible", 0);
         if (visible) {
+
+#ifdef GDK_WINDOWING_WAYLAND
+            // Hack to prevent crash with Wayland. See: https://gitlab.com/inkscape/inkscape/issues/454
+            if (iter->first == "InkscapePreferences") {
+                Glib::ustring session_type  = Glib::getenv("XDG_SESSION_TYPE");  // Window session
+                Glib::ustring session_type2 = Glib::getenv("GDK_BACKEND");       // Possible override
+                if (session_type == "wayland" && session_type2 != "x11") {
+                    std::cerr << "SPDesktop::show_dialog: Cannot restore InkscapePreferences dialog due to GTK Wayland bug." << std::endl;
+                    continue;
+                }
+            }
+#endif
 
             // Try to ensure that the panel is created attached to the correct desktop (bug 1720096).
             // There must be a better way of handling this problem!
