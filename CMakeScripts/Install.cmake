d09679bf26da5702519d4ecfd5303e2155241dfa
Patrick Storz <eduard.braun2@gmx.de>
2019-12-20 17:00:43 +0100

    CMake: Move installation of AUTHORS/TRANSLATORS into install.cmake

--- CMakeScripts/Install.cmake
+++ CMakeScripts/Install.cmake
@@ -1,5 +1,14 @@
 if(UNIX)
-    #The install directive for the binaries and libraries are found in src/CMakeList.txt
+    # The install directive for the binaries and libraries are found in src/CMakeList.txt
+
+    install(FILES
+        AUTHORS
+        TRANSLATORS
+        DESTINATION ${INKSCAPE_SHARE_INSTALL}/doc)
+
+    # TODO: install license
+
+    # desktop file / appdata / etc.
     install(FILES
       ${CMAKE_BINARY_DIR}/org.inkscape.Inkscape.desktop
       DESTINATION ${SHARE_INSTALL}/applications)
