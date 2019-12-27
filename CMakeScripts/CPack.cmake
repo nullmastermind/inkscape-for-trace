e4e0282398a700b6153be8019d5325fbd872d12e
Patrick Storz <eduard.braun2@gmx.de>
2019-12-27 22:32:34 +0100

    CPack: Simplify WIX-versioning
    Seems to work just fine when omitting the artificial build number,
    so drop it.

--- CMakeScripts/CPack.cmake
+++ CMakeScripts/CPack.cmake
@@ -18,27 +18,6 @@ if(CPACK_GENERATOR STREQUAL "NSIS")
     set(CPACK_NSIS_MUI_WELCOMEFINISHPAGE_BITMAP "${CPACK_NSIS_MUI_WELCOMEFINISHPAGE_BITMAP_NATIVE}")
 
 elseif(CPACK_GENERATOR STREQUAL "WIX")
-
-  # for Wix, version should be a.b.c.d with a,b,c between 0 and 255, and
-  # 0<d<65536.
-  # We are using the 4th number to store alpha, beta, or rc information
-
-  string(FIND ${INKSCAPE_VERSION_SUFFIX} "alpha" ISALPHA)
-  string(FIND ${INKSCAPE_VERSION_SUFFIX} "beta" ISBETA)
-  string(FIND ${INKSCAPE_VERSION_SUFFIX} "rc" ISRC)
-  string(REGEX REPLACE "[^0-9]" "" WIX_SUFFIX ${INKSCAPE_VERSION_SUFFIX} )
-
-  if(ISALPHA GREATER -1)
-    math(EXPR WIX_SUFFIX "${WIX_SUFFIX} + 10")
-  elseif(ISBETA GREATER -1)
-    math(EXPR WIX_SUFFIX "${WIX_SUFFIX} + 20")
-  elseif(ISRC GREATER -1)
-    math(EXPR WIX_SUFFIX "${WIX_SUFFIX} + 30")
-  else()
-    math(EXPR WIX_SUFFIX "${WIX_SUFFIX} + 100")
-  endif()
-
-  set(CPACK_PACKAGE_VERSION
-    "${CPACK_PACKAGE_VERSION_MAJOR}.${CPACK_PACKAGE_VERSION_MINOR}.${CPACK_PACKAGE_VERSION_PATCH}.${WIX_SUFFIX}")
-   message("WIX version: ${CPACK_PACKAGE_VERSION}")
+    # for wix version values need to look like 'x.x.x.x' where x is an integer from 0 to 65534.
+    set(CPACK_PACKAGE_VERSION "${CPACK_PACKAGE_VERSION_MAJOR}.${CPACK_PACKAGE_VERSION_MINOR}.${CPACK_PACKAGE_VERSION_PATCH}")
 endif()
