adc40883a70a98e4a6b69c465ba3a67205262525
Tavmjong Bah <tavmjong@free.fr>
2020-01-21 10:12:02 +0100

    Implement a number of additional select/unselect actions.

--- src/actions/actions-selection.cpp
+++ src/actions/actions-selection.cpp
@@ -19,6 +19,9 @@
 #include "inkscape.h"             // Inkscape::Application
 #include "selection.h"            // Selection
 
+#include "object/sp-root.h"       // select_all: document->getRoot();
+#include "object/sp-item-group.h" // select_all
+
 void
 select_clear(InkscapeApplication* app)
 {
@@ -46,6 +49,26 @@ select_via_id(Glib::ustring ids, InkscapeApplication* app)
             selection->add(object);
         } else {
             std::cerr << "select_via_id: Did not find object with id: " << id << std::endl;
+        }
+    }
+}
+
+void
+unselect_via_id(Glib::ustring ids, InkscapeApplication* app)
+{
+    SPDocument* document = nullptr;
+    Inkscape::Selection* selection = nullptr;
+    if (!get_document_and_selection(app, &document, &selection)) {
+        return;
+    }
+
+    auto tokens = Glib::Regex::split_simple("\\s*,\\s*", ids);
+    for (auto id : tokens) {
+        SPObject* object = document->getObjectById(id);
+        if (object) {
+            selection->remove(object);
+        } else {
+            std::cerr << "unselect_via_id: Did not find object with id: " << id << std::endl;
         }
     }
 }
@@ -88,16 +111,125 @@ select_via_selector(Glib::ustring selector, InkscapeApplication* app)
     selection->add(objects.begin(), objects.end());
 }
 
+
+// Helper
 void
-select_all(InkscapeApplication* app)
+get_all_items_recursive(std::vector<SPObject *> &objects, SPObject *object, Glib::ustring &condition)
+{
+    for (auto &o : object->childList(false)) {
+        if (dynamic_cast<SPItem *>(o)) {
+            SPGroup *group = dynamic_cast<SPGroup *>(o);
+            if (condition == "layers") {
+                if (group && group->layerMode() == SPGroup::LAYER) {
+                    objects.emplace_back(o);
+                    continue; // Layers cannot contain layers.
+                }
+            } else if (condition == "no-layers") {
+                if (group && group->layerMode() == SPGroup::LAYER) {
+                    // recurse one level
+                } else {
+                    objects.emplace_back(o);
+                    continue;
+                }
+            } else if (condition == "groups") {
+                if (group) {
+                    objects.emplace_back(o);
+                }
+            } else if (condition == "all") {
+                objects.emplace_back(o);
+            } else {
+                // no-groups, default
+                if (!group) {
+                    objects.emplace_back(o);
+                    continue; // Non-groups cannot contain items.
+                }
+            }
+            get_all_items_recursive(objects, o, condition);
+        }
+    }
+}
+
+
+/*
+ * 'layers':            All layers.
+ * 'groups':            All groups (including layers).
+ * 'no-layers':         All top level objects in all layers (matches GUI "Select All in All Layers").
+ * 'no-groups':         All objects other than groups (and layers).
+ * 'all':               All objects including groups and their decendents.
+ *
+ * Note: GUI "Select All" requires knowledge of selected layer, which is a desktop property.
+ */
+void
+select_all(Glib::ustring condition, InkscapeApplication* app)
+{
+    if (condition != "" && condition != "layers" && condition != "no-layers" &&
+        condition != "groups" && condition != "no-groups" && condition != "all") {
+        std::cerr << "select_all: allowed options are '', 'all', 'layers', 'no-layers', 'groups', and 'no-groups'" << std::endl;
+        return;
+    }
+
+    SPDocument* document = nullptr;
+    Inkscape::Selection* selection = nullptr;
+    if (!get_document_and_selection(app, &document, &selection)) {
+        return;
+    }
+
+    std::vector<SPObject *> objects;
+    get_all_items_recursive(objects, document->getRoot(), condition);
+
+    selection->setList(objects);
+}
+
+/* See above for conditions. */
+void
+select_invert(Glib::ustring condition, InkscapeApplication* app)
+{
+    if (condition != "" && condition != "layers" && condition != "no-layers" &&
+        condition != "groups" && condition != "no-groups" && condition != "all") {
+        std::cerr << "select_all: allowed options are '', 'all', 'layers', 'no-layers', 'groups', and 'no-groups'" << std::endl;
+        return;
+    }
+
+    SPDocument* document = nullptr;
+    Inkscape::Selection* selection = nullptr;
+    if (!get_document_and_selection(app, &document, &selection)) {
+        return;
+    }
+
+    // Find all objects that match condition.
+    std::vector<SPObject *> objects;
+    get_all_items_recursive(objects, document->getRoot(), condition);
+
+    // Get current selection.
+    std::vector<SPObject *> current(selection->items().begin(), selection->items().end());
+
+    // Remove current selection from object vector (using "erase remove_if idiom").
+    objects.erase(
+        std::remove_if(std::begin(objects), std::end(objects), [&current](const SPObject *x)
+            {
+                return (std::find(current.begin(), current.end(), x) != current.end());
+            }), objects.end());
+
+    // Set selection to object vector.
+    selection->setList(objects);
+}
+
+// Debug... print selected items
+void
+select_list(InkscapeApplication* app)
 {
     SPDocument* document = nullptr;
     Inkscape::Selection* selection = nullptr;
     if (!get_document_and_selection(app, &document, &selection)) {
         return;
     }
-    std::cerr << "select_all: Not implemented!" << std::endl;
+
+    auto items = selection->items();
+    for (auto i = items.begin(); i != items.end(); ++i) {
+        std::cout << **i << std::endl;
+    }
 }
+
 
 template<class T>
 void
@@ -105,11 +237,15 @@ add_actions_selection(ConcreteInkscapeApplication<T>* app)
 {
     app->add_action(               "select-clear",       sigc::bind<InkscapeApplication*>(sigc::ptr_fun(&select_clear),              app)        );
     app->add_action_radio_string(  "select",             sigc::bind<InkscapeApplication*>(sigc::ptr_fun(&select_via_id),             app), "null"); // Backwards compatible.
+    app->add_action_radio_string(  "unselect",           sigc::bind<InkscapeApplication*>(sigc::ptr_fun(&unselect_via_id),           app), "null"); // Match select.
     app->add_action_radio_string(  "select-via-id",      sigc::bind<InkscapeApplication*>(sigc::ptr_fun(&select_via_id),             app), "null");
+    app->add_action_radio_string(  "unselect-via-id",    sigc::bind<InkscapeApplication*>(sigc::ptr_fun(&unselect_via_id),           app), "null");
     app->add_action_radio_string(  "select-via-class",   sigc::bind<InkscapeApplication*>(sigc::ptr_fun(&select_via_class),          app), "null");
     app->add_action_radio_string(  "select-via-element", sigc::bind<InkscapeApplication*>(sigc::ptr_fun(&select_via_element),        app), "null");
     app->add_action_radio_string(  "select-via-selector",sigc::bind<InkscapeApplication*>(sigc::ptr_fun(&select_via_selector),       app), "null");
-    app->add_action(               "select-all",         sigc::bind<InkscapeApplication*>(sigc::ptr_fun(&select_all),                app)        );
+    app->add_action_radio_string(  "select-all",         sigc::bind<InkscapeApplication*>(sigc::ptr_fun(&select_all),                app), "null");
+    app->add_action_radio_string(  "select-invert",      sigc::bind<InkscapeApplication*>(sigc::ptr_fun(&select_invert),             app), "null");
+    app->add_action(               "select-list",        sigc::bind<InkscapeApplication*>(sigc::ptr_fun(&select_list),               app)        );
 }
 
 template void add_actions_selection(ConcreteInkscapeApplication<Gio::Application>* app);
