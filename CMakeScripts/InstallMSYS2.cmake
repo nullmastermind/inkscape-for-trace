46974eb3cfd400c76a934e3b19fec13c8e1e8db2
Patrick Storz <eduard.braun2@gmx.de>
2020-01-28 19:59:44 +0100

    CMake/MSYS2: add new dependency (curl now depends on libssh)

--- CMakeScripts/InstallMSYS2.cmake
+++ CMakeScripts/InstallMSYS2.cmake
@@ -103,6 +103,7 @@ if(WIN32)
     ${MINGW_BIN}/libsigc-2.0-[0-9]*.dll
     ${MINGW_BIN}/libsoup-2.4-[0-9]*.dll
     ${MINGW_BIN}/libsqlite3-[0-9]*.dll
+    ${MINGW_BIN}/libssh2-[0-9]*.dll
     ${MINGW_BIN}/libssl-1_[0-9]*.dll
     ${MINGW_BIN}/libssp-[0-9]*.dll
     ${MINGW_BIN}/libstdc++-[0-9]*.dll
