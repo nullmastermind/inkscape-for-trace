4b04e39d0b61fdaea0c909e69738bbdce9763210
Thomas Holder <thomas@thomas-holder.de>
2020-02-19 08:05:31 +0100

    fix CI: skip ASSERT_DEATH on Windows

--- testfiles/src/2geom-characterization-test.cpp
+++ testfiles/src/2geom-characterization-test.cpp
@@ -17,7 +17,9 @@ TEST(Characterization2Geom, retrievingBackElementOfAnEmptyClosedPathFails)
     path.close();
     ASSERT_TRUE(path.closed());
     ASSERT_EQ(path.size_closed(), 0u);
+#ifndef _WIN32
     ASSERT_DEATH(path.back_closed(), "");
+#endif
 }
 
 /*
