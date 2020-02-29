952cbafb2b4663b35b819662c55e482711b34b37
Patrick Storz <eduard.braun2@gmx.de>
2020-02-29 16:41:49 +0100

    Revert "Clipboard: Fix pasting of image data on Windows"
    This reverts commit 85f8a8b35fc04fc550ca02578092b5580910aea5.
    
    Fixed upstream, see
    - https://gitlab.gnome.org/GNOME/gtk/issues/2223
    - https://gitlab.gnome.org/GNOME/gtk/-/merge_requests/1198
    - https://gitlab.gnome.org/GNOME/gtk/commit/6452d34cd7d84ee8b1edb784a9a08310d1b720b2

--- src/ui/clipboard.cpp
+++ src/ui/clipboard.cpp
@@ -1065,13 +1065,6 @@ bool ClipboardManagerImpl::_pasteImage(SPDocument *doc)
 
     // retrieve image data
     Glib::RefPtr<Gdk::Pixbuf> img = _clipboard->wait_for_image();
-#ifdef _WIN32
-    // For some reason the first call to wait_for_image() often fails, despite image data being available
-    // TODO: Figure out why that is and remove this hack.
-    if (!img) {
-        img = _clipboard->wait_for_image();
-    }
-#endif
     if (!img) {
         return false;
     }
