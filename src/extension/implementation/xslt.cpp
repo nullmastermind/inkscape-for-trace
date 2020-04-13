50319a7a3c6b5c9cec9331f2f45811b2d330918a
Jonathan Hofinger <jonathan.hofinger@gmx.de>
2020-04-13 08:38:24 +0000

    Workaround for inbox#2208 - temporarily reset locale during XAML export

--- src/extension/implementation/xslt.cpp
+++ src/extension/implementation/xslt.cpp
@@ -34,6 +34,7 @@
 #include "xml/node.h"
 #include "xml/repr.h"
 
+#include <clocale>
 
 Inkscape::XML::Document * sp_repr_do_read (xmlDocPtr doc, const gchar * default_ns);
 
@@ -114,12 +115,16 @@ SPDocument * XSLT::open(Inkscape::Extension::Input */*module*/,
 
     const char * params[1];
     params[0] = nullptr;
+    char *oldlocale = g_strdup(std::setlocale(LC_NUMERIC, nullptr));
+    std::setlocale(LC_NUMERIC, "C");
 
     xmlDocPtr result = xsltApplyStylesheet(_stylesheet, filein, params);
     xmlFreeDoc(filein);
 
     Inkscape::XML::Document * rdoc = sp_repr_do_read( result, SP_SVG_NS_URI);
     xmlFreeDoc(result);
+    std::setlocale(LC_NUMERIC, oldlocale);
+    g_free(oldlocale);
 
     if (rdoc == nullptr) {
         return nullptr;
@@ -208,9 +213,14 @@ void XSLT::save(Inkscape::Extension::Output *module, SPDocument *doc, gchar cons
     }
     xslt_params[count] = nullptr;
 
+    // workaround for inbox#2208
+    char *oldlocale = g_strdup(std::setlocale(LC_NUMERIC, nullptr));
+    std::setlocale(LC_NUMERIC, "C");
     xmlDocPtr newdoc = xsltApplyStylesheet(_stylesheet, svgdoc, xslt_params);
     //xmlSaveFile(filename, newdoc);
     int success = xsltSaveResultToFilename(filename, newdoc, _stylesheet, 0);
+    std::setlocale(LC_NUMERIC, oldlocale);
+    g_free(oldlocale);
 
     xmlFreeDoc(newdoc);
     xmlFreeDoc(svgdoc);
