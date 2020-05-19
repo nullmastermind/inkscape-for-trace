affcaa7cd39a182e506096bb4f566839065935fd
Brett T. Warden <4c0e8e88@tm.wgz.org>
2020-05-19 18:11:08 +0000

    Copy all translation files before using them
    The translation file es_MX.po has a fuzzy attribute for the msgid "Label", so when evaluating this translation, gettext will fall through to language 'es' for the translation. If po/locale/es/LC_MESSAGES/inkscape.mo does not exist at the time of the call, the translation will fail, and "Label" will be returned, causing the check to fail, and preventing writing es_MX.svg.
    
    The order in which languages are processed is determined by the results of glob.glob, which may vary with filesystem type. In some build environments, 'es' is processed before 'es_MX', and no problem occurs. This is not guaranteed, however. In other build environments, 'es_MX' occurs first, and lacking 'es', the translation fails, and es_MX.svg is not generated.
    
    To remove reliance on a particular ordering of gmofiles, and ensure build reprodicibility, process the translations in two complete passes -- one to copy the gmo files, and when complete, the second to actually use the translations to generate the svg files.

--- share/templates/create_default_templates.py
+++ share/templates/create_default_templates.py
@@ -44,6 +44,8 @@ for language in languages:
         os.makedirs(destination_dir)
     shutil.copy(source, destination)
 
+# do another loop to ensure we've copied all the translations before using them
+for language in languages:
     # get translation with help of gettext
     translation = gettext.translation('inkscape', localedir=binary_dir + '/po/locale', languages=[language])
     translated_string = translation.gettext(LAYER_STRING)
