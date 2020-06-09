41b0cdaaebb1bbdbdd0fcba92bd7b8c6bc86c426
Thomas Holder <thomas@thomas-holder.de>
2020-06-09 20:19:53 +0200

    AppImage: Add Python symlinks

--- packaging/appimage/generate.sh
+++ packaging/appimage/generate.sh
@@ -26,33 +26,47 @@ cd appdir/
 # to allow the AppImage to run on older systems as well
 ########################################################################
 
+apt_bundle() {
+    apt-get download "$@"
+    find *.deb -exec dpkg-deb -x {} . \;
+    find *.deb -delete
+}
+
 # Bundle all of glibc; this should eventually be done by linuxdeployqt
 apt update
-apt-get download libc6
-find *.deb -exec dpkg-deb -x {} . \;
-rm *deb
+apt_bundle libc6
 
 # Make absolutely sure it will not load stuff from /lib or /usr
 sed -i -e 's|/usr|/xxx|g' lib/x86_64-linux-gnu/ld-linux-x86-64.so.2
-sed -i -e 's|/usr/lib|/ooo/ooo|g' lib/x86_64-linux-gnu/ld-linux-x86-64.so.2
 
 # Bundle Gdk pixbuf loaders without which the bundled Gtk does not work;
 # this should eventually be done by linuxdeployqt
-apt-get download librsvg2-common libgdk-pixbuf2.0-0
-find *.deb -exec dpkg-deb -x {} . \;
-rm *deb
+apt_bundle librsvg2-common libgdk-pixbuf2.0-0
 cp /usr/lib/x86_64-linux-gnu/gdk-pixbuf-*/*/loaders/* usr/lib/x86_64-linux-gnu/gdk-pixbuf-*/*/loaders/
 cp /usr/lib/x86_64-linux-gnu/gdk-pixbuf-*/*/loaders.cache usr/lib/x86_64-linux-gnu/gdk-pixbuf-*/*/
-sed -i -e 's|/usr/lib/x86_64-linux-gnu/gdk-pixbuf-2.0/2.10.0/loaders/||g' usr/lib/x86_64-linux-gnu/gdk-pixbuf-*/*/loaders.cache
+sed -i -e 's|/usr/lib/x86_64-linux-gnu/gdk-pixbuf-.*/.*/loaders/||g' usr/lib/x86_64-linux-gnu/gdk-pixbuf-*/*/loaders.cache
 
 # Bundle fontconfig settings
 mkdir -p etc/fonts/
 cp /etc/fonts/fonts.conf etc/fonts/
 
 # Bundle Python
-apt-get download libpython3.8-stdlib python3.8 python3.8-minimal libpython3.8-minimal python3-lxml python3-numpy python3-scour python3-gi
-find *.deb -exec dpkg-deb -x {} . \;
-rm *deb
+PY_VER=3.8
+apt_bundle \
+    libpython${PY_VER}-stdlib \
+    libpython${PY_VER}-minimal \
+    python${PY_VER} \
+    python${PY_VER}-minimal \
+    python3-lxml \
+    python3-numpy \
+    python3-scour \
+    python3-gi
+(
+    cd usr/bin
+    ln -s python${PY_VER} python3
+    ln -s python${PY_VER} python
+)
+
 cd -
 
 ########################################################################
@@ -67,7 +81,7 @@ linuxdeployqtargs=()
 for so in $(find \
     appdir/usr/lib/x86_64-linux-gnu/gdk-pixbuf-*/*/loaders \
     appdir/usr/lib/python3 \
-    appdir/usr/lib/python3.8 \
+    appdir/usr/lib/python${PY_VER} \
     -name \*.so); do
     linuxdeployqtargs+=("-executable=$(readlink -f "$so")")
 done
@@ -75,7 +89,7 @@ done
 ./linuxdeployqt-continuous-x86_64.AppImage --appimage-extract-and-run appdir/usr/share/applications/org.inkscape.Inkscape.desktop \
   -appimage -unsupported-bundle-everything -executable=appdir/usr/bin/inkview \
   -executable=appdir/usr/lib/inkscape/libinkscape_base.so \
-  -executable=appdir/usr/bin/python3.8 \
+  -executable=appdir/usr/bin/python${PY_VER} \
   "${linuxdeployqtargs[@]}"
 
 mv Inkscape*.AppImage* ../
