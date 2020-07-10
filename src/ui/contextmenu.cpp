3cce3c2d6a28c5f3424e5d89cc9d9e13f4708cdc
Shubham <aryan10jangid@gmail.com>
2020-07-10 04:40:27 +0000

    Right-click "enter group" command should display group label, not group id
    Closes https://gitlab.com/inkscape/inkscape/-/issues/1545
    Closes https://gitlab.com/inkscape/inkscape/-/merge_requests/2133

--- src/ui/contextmenu.cpp
+++ src/ui/contextmenu.cpp
@@ -160,8 +160,7 @@ ContextMenu::ContextMenu(SPDesktop *desktop, SPItem *item) :
     }
 
     if ( group && group != _desktop->currentLayer() ) {
-        /* TRANSLATORS: #%1 is the id of the group e.g. <g id="#g7">, not a number. */
-        MIGroup.set_label (Glib::ustring::compose(_("Enter group #%1"), group->getId()));
+        MIGroup.set_label (Glib::ustring::compose(_("Enter group %1"), group->defaultLabel()));
         _MIGroup_group = group;
         MIGroup.signal_activate().connect(sigc::bind(sigc::mem_fun(*this, &ContextMenu::EnterGroup),&MIGroup));
         MIGroup.show();
