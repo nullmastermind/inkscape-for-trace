7427fc635e0578ba3e9dcee7226b214701610d24
Tavmjong Bah <tavmjong@free.fr>
2020-09-09 13:36:16 +0200

    Attempt to fix translations CI check for .ui files.

--- po/generate_POTFILES.sh
+++ po/generate_POTFILES.sh
@@ -23,4 +23,9 @@ export LC_ALL=C
 
 find ../share/extensions -name '*.py' -type f -print0 | xargs -0 egrep -l '(\<[QNC]?_|gettext) *\(' | grep -vx -f POTFILES.skip | sort > POTFILES.py.in
 find ../share/extensions -name '*.inx' -type f -print | grep -vx -f POTFILES.skip | sort > POTFILES.inx.in
-find ../share/ui -name '*.glade' -type f -print | grep -vx -f POTFILES.skip | sort > POTFILES.ui.in
+
+# Keep .glade files and .ui files separated.
+(
+    find ../share/ui -name  '*.glade' -type f -print0 | xargs -0 egrep -l 'translatable' | sort
+    find ../share/ui -name  '*.ui'    -type f -print0 | xargs -0 egrep -l 'translatable' | sort
+) | grep -vx -f POTFILES.skip > POTFILES.ui.in
