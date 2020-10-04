f6e8995bec514cdd1565bfaf690b6c4b14682aaf
Martin Owens <doctormo@gmail.com>
2020-10-04 18:11:57 -0400

    Repair duplicate variable from merge

--- src/ui/dialog/inkscape-preferences.h
+++ src/ui/dialog/inkscape-preferences.h
@@ -396,7 +396,6 @@ protected:
     UI::Widget::PrefSpinButton  _misc_recent;
     UI::Widget::PrefCheckButton _ui_realworldzoom;
     UI::Widget::PrefCheckButton _ui_partialdynamic;
-    UI::Widget::PrefCheckButton _ui_realworldzoom;
     UI::Widget::ZoomCorrRulerSlider _ui_zoom_correction;
     UI::Widget::PrefCheckButton _ui_yaxisdown;
     UI::Widget::PrefCheckButton _ui_rotationlock;
