nullmastermind/inkscape-for-trace

commit 43a04b069e2b411c1f92365268caf293e9e51ba7

Rewrite shortcut code to handle accelerators for Gio::Actions.
Changes include:
* Converting to C++ class.
* Removing duplicate code.
* Removing dead code.
* Fixing memory leak.
* Eliminating improper use of GDK_MODIFIER_RESERVED_XXX values.

To do:
* Handle window and document level Gio::Actions
* Integrate verbs and actions in preferences.

src/ui/shortcuts.h
@@ -1,0 +1,123 @@
+// SPDX-License-Identifier: GPL-2.0-or-later
+/*
+ * Shortcuts
+ *
+ * Copyright (C) 2020 Tavmjong Bah
+ *
+ * The contents of this file may be used under the GNU General Public License Version 2 or later.
+ *
+ */
+
+#ifndef INK_SHORTCUTS_H
+#define INK_SHORTCUTS_H
+
+#include <map>
+
+#include <giomm.h>
+#include <gtkmm.h>
+
+namespace Inkscape {
+class Verb;
+
+namespace UI {
+namespace View {
+class View;
+}
+}
+
+namespace XML {
+class Document;
+class Node;
+}
+
+class Shortcuts {
+
+public:
+
+    enum What {
+        All,
+        System,
+        User
+    };
+        
+    static Shortcuts& getInstance()
+    {
+        static Shortcuts instance;
+        return instance;
+    }
+  
+private:
+    Shortcuts();
+    ~Shortcuts() = default;
+
+public:
+    Shortcuts(Shortcuts const&)      = delete;
+    void operator=(Shortcuts const&) = delete;
+
+    void init();
+    void clear();
+
+    bool read( Glib::RefPtr<Gio::File> file, bool user_set = false);
+    bool write(Glib::RefPtr<Gio::File> file, What what = User);
+
+    // Will disappear after verbs are gone.
+    void set_verb_shortcut(unsigned long long int const &val, Inkscape::Verb *const verb, bool is_primary, bool is_user_set);
+    unsigned long long int get_shortcut_from_verb(Verb* verb);
+    Verb* get_verb_from_shortcut(unsigned long long int shortcut);
+    bool invoke_verb(GdkEventKey const *event, UI::View::View *view);
+
+    bool is_user_set(Verb* verb);
+    bool is_user_set(Glib::ustring& action);
+
+    // User shortcuts
+    bool add_user_shortcut(Glib::ustring name, unsigned long long int shortcut);
+    bool remove_user_shortcut(Glib::ustring name, unsigned long long int shortcut);
+    bool clear_user_shortcuts();
+
+    // Utility
+    static Glib::ustring get_label(unsigned long long int shortcut);
+    static Glib::ustring get_modifiers_verb(unsigned int mod_val);
+    static unsigned long long int get_from_event(GdkEventKey const *event);
+    
+    // Will disappear after verbs are gone. (Use Gtk::AccelGroup functions instead for actions.)
+    Glib::ustring shortcut_to_accelerator(unsigned long long int shortcut);
+    unsigned long long int accelerator_to_shortcut(Glib::ustring& accelerator);
+
+    // Will disappear after verbs are gone. 
+    void add_accelerator(Gtk::Widget *widget, Verb* verb);
+
+    static std::vector<std::pair<Glib::ustring, Glib::ustring>> get_file_names();
+
+    // Dialogs
+    bool import_shortcuts();
+    bool export_shortcuts();
+
+    // Debug
+    void dump();
+
+private:
+
+    // Gio::Actions
+    Glib::RefPtr<Gtk::Application> app;
+    std::map<Glib::ustring, bool> action_user_set;
+
+    //Legacy verbs
+    std::map<unsigned long long int, Inkscape::Verb*> shortcut_to_verb_map;
+    std::map<Inkscape::Verb *, unsigned long long int> primary;  // Shown in menus, etc.
+    std::map<Inkscape::Verb *, bool> user_set;
+};
+
+} // Namespace Inkscape
+
+#endif // INK_SHORTCUTS_H
+
+/*
+  Local Variables:
+  mode:c++
+  c-file-style:"stroustrup"
+  c-file-offsets:((innamespace . 0)(inline-open . 0)(case-label . +))
+  indent-tabs-mode:nil
+  fill-column:99
+  End:
+*/
+// vim: filetype=cpp:expandtab:shiftwidth=4:tabstop=8:softtabstop=4 :

src/shortcuts.h
@@ -1,88 +1,0 @@
-// SPDX-License-Identifier: GPL-2.0-or-later
-/** @file
- * TODO: insert short description here
- *//*
- * Authors: see git history
- *
- * Copyright (C) 2017 Authors
- * Released under GNU GPL v2+, read the file 'COPYING' for more information.
- */
-#ifndef SEEN_SP_SHORTCUTS_H
-#define SEEN_SP_SHORTCUTS_H
-
-/*
- * Keyboard shortcut processing
- *
- * Author:
- *   Lauris Kaplinski <lauris@kaplinski.com>
- *
- * This code is in public domain
- */
-
-#include <vector>
-#include <gdkmm/types.h>
-#include <gdk/gdk.h>
-
-
-typedef struct _GtkAccelGroup GtkAccelGroup;
-typedef struct _GtkWidget     GtkWidget;
-
-namespace Inkscape {
-    class Verb;
-    namespace UI {
-        namespace View {
-            class View;
-        }
-    }
-}
-
-/* We define high-bit mask for packing into single int */
-
-#define SP_SHORTCUT_SHIFT_MASK GDK_MODIFIER_RESERVED_20_MASK
-#define SP_SHORTCUT_CONTROL_MASK GDK_MODIFIER_RESERVED_21_MASK
-#define SP_SHORTCUT_ALT_MASK GDK_MODIFIER_RESERVED_22_MASK
-#define SP_SHORTCUT_SUPER_MASK GDK_MODIFIER_RESERVED_23_MASK
-#define SP_SHORTCUT_HYPER_MASK GDK_MODIFIER_RESERVED_24_MASK
-#define SP_SHORTCUT_META_MASK GDK_MODIFIER_RESERVED_25_MASK
-#define SP_SHORTCUT_MODIFIER_MASK (SP_SHORTCUT_SHIFT_MASK|SP_SHORTCUT_CONTROL_MASK|SP_SHORTCUT_ALT_MASK|SP_SHORTCUT_SUPER_MASK|SP_SHORTCUT_HYPER_MASK|SP_SHORTCUT_META_MASK)
-
-
-/* Returns true if action was performed */
-bool sp_shortcut_invoke (unsigned int shortcut, Inkscape::UI::View::View *view);
-
-void sp_shortcut_init();
-void sp_shortcut_ensure_init();
-Inkscape::Verb * sp_shortcut_get_verb (unsigned int shortcut);
-unsigned int sp_shortcut_get_primary (Inkscape::Verb * verb); // Returns GDK_VoidSymbol if no shortcut is found.
-char* sp_shortcut_get_label (unsigned int shortcut); // Returns the human readable form of the shortcut (or NULL), for example Shift+Ctrl+F. Free the returned string with g_free.
-void sp_shortcut_set(unsigned int const shortcut, Inkscape::Verb *const verb, bool const is_primary, bool const is_user_set=false);
-void sp_shortcut_unset(unsigned int const shortcut);
-void sp_shortcut_add_to_file(char const *action, unsigned int const shortcut);
-void sp_shortcut_delete_from_file(char const *action, unsigned int const shortcut);
-void sp_shortcuts_delete_all_from_file();
-Glib::ustring sp_shortcut_to_label(unsigned int const shortcut);
-unsigned int sp_shortcut_get_from_event(GdkEventKey const *event);
-unsigned int sp_shortcut_get_from_gdk_event(unsigned int accel_key, Gdk::ModifierType accel_mods, unsigned int hardware_keycode);
-unsigned int sp_shortcut_get_for_event(GdkEventKey const *event);
-void sp_shortcut_get_file_names(std::vector<Glib::ustring> *names, std::vector<Glib::ustring> *paths);
-bool sp_shortcut_is_user_set(Inkscape::Verb *verb);
-void sp_shortcut_file_export();
-bool sp_shortcut_file_import();
-void sp_shortcut_file_import_do(char const *importname);
-void sp_shortcut_file_export_do(char const *exportname);
-GtkAccelGroup *sp_shortcut_get_accel_group();
-void sp_shortcut_add_accelerator(GtkWidget *item, unsigned int const shortcut);
-guint parse_modifier_string(gchar const *modifiers_string, gchar const *verb_name);
-
-#endif
-
-/*
-  Local Variables:
-  mode:c++
-  c-file-style:"stroustrup"
-  c-file-offsets:((innamespace . 0)(inline-open . 0)(case-label . +))
-  indent-tabs-mode:nil
-  fill-column:99
-  End:
-*/
-// vim: filetype=cpp:expandtab:shiftwidth=4:tabstop=8:softtabstop=4:fileencoding=utf-8:textwidth=99 :

share/keys/inkscape.xml
@@ -657,4 +657,7 @@ override) the bindings in the main default.xml.
   <modifier action="trans-fixed-ratio" modifiers="Ctrl"/>
   <modifier action="trans-off-center" modifiers="Shift"/>
   <modifier action="trans-snapping" modifiers="Shift"/>
+<!-- Actions -->
+   <!-- Test -->
+   <bind gaction="app.inkscape-version" keys="&lt;Shift&gt;v" />
 </keys>

src/CMakeLists.txt
@@ -73,7 +73,6 @@ set(inkscape_SRC
   selection.cpp
   seltrans-handles.cpp
   seltrans.cpp
-  shortcuts.cpp
   snap-preferences.cpp
   snap.cpp
   snapped-curve.cpp
@@ -180,7 +179,6 @@ set(inkscape_SRC
   selection.h
   seltrans-handles.h
   seltrans.h
-  shortcuts.h
   snap-candidate.h
   snap-enums.h
   snap-preferences.h

src/desktop-events.cpp
@@ -55,6 +55,8 @@
 #include "ui/tools/tool-base.h"
 #include "ui/widget/canvas.h"  // Desktop hidden in g_object data.
 
+#include "ui/event-debug.h"
+
 #include "widgets/desktop-widget.h"
 
 #include "xml/repr.h"
@@ -68,6 +70,9 @@ static void init_extended();
 
 bool sp_desktop_root_handler(GdkEvent *event, SPDesktop *desktop)
 {
+#ifdef EVENT_DEBUG
+    ui_dump_event(reinterpret_cast<GdkEvent *>(event), "sp_desktop_root_handler");
+#endif
     static bool watch = false;
     static bool first = true;
 
@@ -99,6 +104,10 @@ static SPGuideDragType drag_type = SP_DRAG_NONE;
 
 bool sp_dt_guide_event(GdkEvent *event, Inkscape::CanvasItemGuideLine *guide_item, SPGuide *guide)
 {
+#ifdef EVENT_DEBUG
+    ui_dump_event(reinterpret_cast<GdkEvent *>(event), "sp_dt_guide_event");
+#endif
+
     static bool moved = false;
     bool ret = false;
 

src/helper/verb-action.cpp
@@ -34,7 +34,6 @@
 #include <glibmm/i18n.h>
 #include <gtkmm/toolitem.h>
 
-#include "shortcuts.h"
 #include "verbs.h"
 #include "helper/action.h"
 #include "ui/widget/button.h"
@@ -66,23 +65,6 @@ GtkToolItem * sp_toolbox_button_item_new_from_verb_with_doubleclick(GtkWidget *t
     b->show();
     auto b_toolitem = Gtk::manage(new Gtk::ToolItem());
     b_toolitem->add(*b);
-
-    unsigned int shortcut = sp_shortcut_get_primary(verb);
-    if (shortcut != GDK_KEY_VoidSymbol) {
-        gchar *key = sp_shortcut_get_label(shortcut);
-        gchar *tip = g_strdup_printf ("%s (%s)", action->tip, key);
-        if ( t ) {
-           gtk_toolbar_insert(GTK_TOOLBAR(t), b_toolitem->gobj(), -1);
-           b->set_tooltip_text(tip);
-        }
-        g_free(tip);
-        g_free(key);
-    } else {
-        if ( t ) {
-            gtk_toolbar_insert(GTK_TOOLBAR(t), b_toolitem->gobj(), -1);
-            b->set_tooltip_text(action->tip);
-        }
-    }
 
     return GTK_TOOL_ITEM(b_toolitem->gobj());
 }

commit 17927dee9bb22d8b6c7e04b9177775c9e2380bbe

Fix reading of illustrator shortcuts
Read nested key files. Regression from 43a04b06
https://gitlab.com/inkscape/inkscape/-/issues/2109

src/ui/shortcuts.cpp
@@ -223,8 +223,22 @@ Shortcuts::read(Glib::RefPtr<Gio::File> file, bool user_set)
         return false;
     }
 
-    // Loop through children of <keys>
-    iter = iter->firstChild();
+    // Loop through the children in <keys> (may have nested keys)
+    _read(*iter, user_set);
+
+    return true;
+}
+
+/**
+ * Recursively reads shortcuts from shortcut file. Does not check for conflicts between verbs and actions.
+ *
+ * @param keysnode The <keys> element. Its child nodes will be processed.
+ * @param user_set true if reading from user shortcut file
+ */
+void
+Shortcuts::_read(XML::Node const &keysnode, bool user_set)
+{
+    XML::NodeConstSiblingIterator iter {keysnode.firstChild()};
     for ( ; iter ; ++iter ) {
 
         if (strcmp(iter->name(), "modifier") == 0) {
@@ -269,7 +283,9 @@ Shortcuts::read(Glib::RefPtr<Gio::File> file, bool user_set)
                 }
             }
             continue;
-
+        } else if (strcmp(iter->name(), "keys") == 0) {
+            _read(*iter, user_set);
+            continue;
         } else if (strcmp(iter->name(), "bind") != 0) {
             // Unknown element, do not complain.
             continue;
@@ -333,8 +349,6 @@ Shortcuts::read(Glib::RefPtr<Gio::File> file, bool user_set)
 
         add_shortcut (verb_name, Gtk::AccelKey(keyval, modifiers), user_set, is_primary);
     }
-
-    return true;
 }
 
 bool

src/ui/shortcuts.h
@@ -133,6 +133,8 @@ private:
     std::map<Inkscape::Verb *, Gtk::AccelKey> primary;  // Shown in menus, etc.
     std::map<Inkscape::Verb *, bool> user_set;
 
+    void _read(XML::Node const &keysnode, bool user_set);
+
     bool initialized = false;
 };
 