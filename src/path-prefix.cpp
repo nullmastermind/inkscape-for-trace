bfb81a613c4b39f6e7b0d0fbb57337fdc61c2451
Thomas Klausner <tk@giga.or.at>
2021-07-02 09:29:42 +0200

    Implement get_program_name() for NetBSD.

--- src/path-prefix.cpp
+++ src/path-prefix.cpp
@@ -20,6 +20,11 @@
 #include <mach-o/dyld.h> // for _NSGetExecutablePath
 #endif
 
+#ifdef __NetBSD__
+#include <sys/types.h>
+#include <sys/sysctl.h>
+#endif
+
 #include <cassert>
 #include <glib.h>
 #include <glibmm.h>
@@ -122,6 +127,15 @@ char const *get_program_name()
         program_name = g_file_read_link("/proc/self/exe", NULL);
         if (!program_name) {
             g_warning("get_program_name() - g_file_read_link failed");
+        }
+#elif defined(__NetBSD__)
+        static const int name[] = {CTL_KERN, KERN_PROC_ARGS, -1, KERN_PROC_PATHNAME};
+        char path[MAXPATHLEN];
+        size_t len = sizeof(path);
+        if (sysctl(name, __arraycount(name), path, &len, NULL, 0) == 0) {
+            program_name = realpath(path, nullptr);
+        } else {
+            g_warning("get_program_name() - sysctl failed");
         }
 #else
 #warning get_program_name() - no known way to obtain executable name on this platform
