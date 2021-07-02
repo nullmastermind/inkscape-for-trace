160dc740c66c72e8c739f053cc46f438e2109909
Thomas Klausner <tk@giga.or.at>
2021-07-02 09:29:21 +0200

    Make sure to use the proper pow()

--- src/svg/svg-length.cpp
+++ src/svg/svg-length.cpp
@@ -22,6 +22,8 @@
 #include "stringstream.h"
 #include "util/units.h"
 
+using std::pow;
+
 static unsigned sp_svg_length_read_lff(gchar const *str, SVGLength::Unit *unit, float *val, float *computed, char **next);
 
 #ifndef MAX
