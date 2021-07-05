06b1437e39fd7855dee1b9fa305f35c39f8ab23a
Tobias C. Berner <tcberner@gmail.com>
2021-07-05 12:45:26 +0000

    Add FreeBSD support

--- src/path-prefix.cpp
+++ src/path-prefix.cpp
@@ -25,6 +25,12 @@
 #include <sys/sysctl.h>
 #endif
 
+#ifdef __FreeBSD__
+#include <sys/param.h>
+#include <sys/types.h>
+#include <sys/sysctl.h>
+#endif
+
 #include <cassert>
 #include <glib.h>
 #include <glibmm.h>
@@ -137,6 +143,15 @@ char const *get_program_name()
         } else {
             g_warning("get_program_name() - sysctl failed");
         }
+#elif defined(__FreeBSD__)
+       int mib[4] = { CTL_KERN, KERN_PROC, KERN_PROC_PATHNAME, -1 };
+       char buf[MAXPATHLEN];
+       size_t cb = sizeof(buf);
+       if (sysctl(mib, 4, buf, &cb, NULL, 0) == 0) {
+           program_name = realpath(buf, nullptr);
+       } else {
+           g_warning("get_program_name() - sysctl failed");
+       }      
 #else
 #warning get_program_name() - no known way to obtain executable name on this platform
         g_info("get_program_name() - no known way to obtain executable name on this platform");
