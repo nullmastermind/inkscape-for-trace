6a878ab757b500ad1c4b1b784c5b521b16a40b17
Aaron <ozrendev@protonmail.com>
2021-10-15 18:16:10 +0200

    Fix Gaussian Blur value in Filter Editor
    (cherry picked from commit ba05a4eb9c738b6b1b3b9379f89edc282dd7f48b)

--- src/ui/dialog/filter-effects-dialog.cpp
+++ src/ui/dialog/filter-effects-dialog.cpp
@@ -2830,7 +2830,7 @@ void FilterEffectsDialog::init_settings_widgets()
     _settings->add_spinscale(1, SPAttr::FLOOD_OPACITY, _("Opacity:"), 0, 1, 0.1, 0.01, 2);
 
     _settings->type(NR_FILTER_GAUSSIANBLUR);
-    _settings->add_dualspinscale(SPAttr::STDDEVIATION, _("Standard Deviation:"), 0.01, 100, 1, 0.01, 2, _("The standard deviation for the blur operation."));
+    _settings->add_dualspinscale(SPAttr::STDDEVIATION, _("Standard Deviation:"), 0, 100, 1, 0.01, 2, _("The standard deviation for the blur operation."));
 
     _settings->type(NR_FILTER_MERGE);
     _settings->add_no_params();
