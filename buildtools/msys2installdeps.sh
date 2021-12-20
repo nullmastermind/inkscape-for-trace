07115798eafd3f302dd4b8c9ea956e70b94e1fe4
Marc Jeanmougin <marc@jeanmougin.fr>
2021-12-20 19:54:43 +0100

    hack gettext libintl to allow compilation

--- buildtools/msys2installdeps.sh
+++ buildtools/msys2installdeps.sh
@@ -113,3 +113,24 @@ for arch in $(eval echo $ARCH); do
       ;;
   esac
 done
+
+
+# gettext hack - to remove once gettext has the match
+function hack_libintl(){
+f=/$1/include/libintl.h
+sed -i '/^extern int sprintf/a #ifdef __cplusplus\nnamespace std { using ::libintl_sprintf; }\n#endif' $f
+cat $f
+}
+
+case "$MSYSTEM" in
+  MINGW32)
+    hack_libintl mingw32
+    ;;
+  MINGW64)
+    hack_libintl mingw64
+    ;;
+  *)
+    hack_libintl mingw32
+    hack_libintl mingw64
+    ;;
+esac
